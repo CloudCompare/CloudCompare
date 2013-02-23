7dbba678f5dab128e9ba94d0617f94dc72806c4d
CloudCompare admin <cloudcompare@danielgm.net>
2013-02-23 22:39:17 +0100

    Primitive factory added

---
+++ qCC/ccPrimitiveFactoryDlg.cpp
@@ -0,0 +1,125 @@
+//##########################################################################
+//#                                                                        #
+//#                            CLOUDCOMPARE                                #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU General Public License as published by  #
+//#  the Free Software Foundation; version 2 of the License.               #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#          COPYRIGHT: EDF R&D / TELECOM ParisTech (ENST-TSI)             #
+//#                                                                        #
+//##########################################################################
+//
+//*********************** Last revision of this file ***********************
+//$Author::                                                                $
+//$Rev::                                                                   $
+//$LastChangedDate::                                                       $
+//**************************************************************************
+//
+
+#include "ccPrimitiveFactoryDlg.h"
+
+//qCC
+#include <MainWindow.h>
+
+//qCC_db
+#include <ccGenericPrimitive.h>
+#include <ccPlane.h>
+#include <ccBox.h>
+#include <ccSphere.h>
+#include <ccCylinder.h>
+#include <ccCone.h>
+#include <ccTorus.h>
+#include <ccDish.h>
+
+//system
+#include <assert.h>
+
+ccPrimitiveFactoryDlg::ccPrimitiveFactoryDlg(MainWindow* win)
+	: QDialog(win)
+	, Ui::PrimitiveFactoryDlg()
+	, m_win(win)
+{
+	assert(m_win);
+
+	setupUi(this);	
+
+	connect(createPushButton, SIGNAL(clicked()), this, SLOT(createPrimitive()));
+	connect(closePushButton, SIGNAL(clicked()), this, SLOT(accept()));
+}
+
+void ccPrimitiveFactoryDlg::createPrimitive()
+{
+	if (!m_win)
+		return;
+
+	ccGenericPrimitive* primitive = 0;
+	switch(tabWidget->currentIndex())
+	{
+		//Plane
+		case 0:
+			{
+				primitive = new ccPlane(planeWidthDoubleSpinBox->value(),
+										planeHeightDoubleSpinBox->value());
+			}
+			break;
+		//Box
+		case 1:
+			{
+				CCVector3 dims(boxDxDoubleSpinBox->value(),
+								boxDyDoubleSpinBox->value(),
+								boxDzDoubleSpinBox->value());
+				primitive = new ccBox(dims);
+			}
+			break;
+		//Sphere
+		case 2:
+			{
+				primitive = new ccSphere(sphereRadiusDoubleSpinBox->value());
+			}
+			break;
+		//Cylinder
+		case 3:
+			{
+				primitive = new ccCylinder( cylRadiusDoubleSpinBox->value(),
+											cylHeightDoubleSpinBox->value());
+			}
+			break;
+		//Cone
+		case 4:
+			{
+				primitive = new ccCone( coneTopRadiusDoubleSpinBox->value(),
+										coneBottomRadiusDoubleSpinBox->value(),
+										coneHeightDoubleSpinBox->value(),
+										snoutGroupBox->isChecked() ? coneXOffsetDoubleSpinBox->value() : 0,
+										snoutGroupBox->isChecked() ? coneYOffsetDoubleSpinBox->value() : 0);
+			}
+			break;
+		//Torus
+		case 5:
+			{
+				primitive = new ccTorus( torusInsideRadiusDoubleSpinBox->value(),
+										torusOutsideRadiusDoubleSpinBox->value(),
+										torusAngleDoubleSpinBox->value()*CC_DEG_TO_RAD,
+										torusRectGroupBox->isChecked(),
+										torusRectGroupBox->isChecked() ? torusRectSectionHeightDoubleSpinBox->value() : 0);
+			}
+			break;
+		//Dish
+		case 6:
+			{
+				primitive = new ccDish( dishRadiusDoubleSpinBox->value(),
+										dishHeightDoubleSpinBox->value(),
+										dishEllipsoidGroupBox->isChecked() ? dishRadius2DoubleSpinBox->value() : 0);
+			}
+			break;
+	}
+
+	if (primitive)
+		m_win->addToDB(primitive);
+}
---
+++ qCC/ccPrimitiveFactoryDlg.h
@@ -0,0 +1,57 @@
+//##########################################################################
+//#                                                                        #
+//#                            CLOUDCOMPARE                                #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU General Public License as published by  #
+//#  the Free Software Foundation; version 2 of the License.               #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#          COPYRIGHT: EDF R&D / TELECOM ParisTech (ENST-TSI)             #
+//#                                                                        #
+//##########################################################################
+//
+//*********************** Last revision of this file ***********************
+//$Author::                                                                $
+//$Rev::                                                                   $
+//$LastChangedDate::                                                       $
+//**************************************************************************
+//
+
+#ifndef CC_PRIMITIVE_FACTORY_DLG_HEADER
+#define CC_PRIMITIVE_FACTORY_DLG_HEADER
+
+#include <ui_primitiveFactoryDlg.h>
+
+//Qt
+#include <QDialog>
+
+class MainWindow;
+
+//! Primitive factory
+class ccPrimitiveFactoryDlg : public QDialog, public Ui::PrimitiveFactoryDlg
+{
+	Q_OBJECT
+
+public:
+
+	//! Default constructor
+	ccPrimitiveFactoryDlg(MainWindow* win);
+
+protected slots:
+
+	//! Creates currently defined primitive
+	void createPrimitive();
+
+protected:
+
+	//! Associated main window
+	MainWindow* m_win;
+
+};
+
+#endif //CC_PRIMITIVE_FACTORY_DLG_HEADER

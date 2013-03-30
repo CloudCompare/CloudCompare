ae1cdb665dd20a1faba77943abcef3930488124c
CloudCompare admin <cloudcompare@danielgm.net>
2013-03-30 23:59:52 +0100

    - New shader for faster dynamic color lookup when displaying scalar fields (only for point clouds for the moment) - Glew updated - ccGLUtils move to qCC_db - default materials and colors slightly updated

---
+++ libs/qCC_db/ccGLUtils.h
@@ -0,0 +1,118 @@
+//##########################################################################
+//#                                                                        #
+//#                            CLOUDCOMPARE                                #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU General Public License as published by  #
+//#  the Free Software Foundation; version 2 of the License.               #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#          COPYRIGHT: EDF R&D / TELECOM ParisTech (ENST-TSI)             #
+//#                                                                        #
+//##########################################################################
+
+#ifndef CC_GL_UTILS_HEADER
+#define CC_GL_UTILS_HEADER
+
+//qCC_db
+#include <ccIncludeGL.h> //Always first!
+#include <ccGLMatrix.h>
+
+//! View orientation
+enum CC_VIEW_ORIENTATION {  CC_TOP_VIEW,	/**< Top view (eye: +Z) **/
+                            CC_BOTTOM_VIEW,	/**< Bottom view **/
+                            CC_FRONT_VIEW,	/**< Front view **/
+                            CC_BACK_VIEW,	/**< Back view **/
+                            CC_LEFT_VIEW,	/**< Left view **/
+                            CC_RIGHT_VIEW,	/**< Right view **/
+                            CC_ISO_VIEW_1,	/**< Isometric view 1: front, right and top **/
+                            CC_ISO_VIEW_2,	/**< Isometric view 2: back, left and top **/
+};
+
+#ifdef QCC_DB_USE_AS_DLL
+#include "qCC_db_dll.h"
+class QCC_DB_DLL_API ccGLUtils
+#else
+class ccGLUtils
+#endif
+{
+public:
+
+	/***************************************************
+                    OpenGL Textures
+	***************************************************/
+
+    static void DisplayTexture2DPosition(GLuint tex, int x, int y, int w, int h, unsigned char alpha=255);
+    static void DisplayTexture2D(GLuint tex, int w, int h, unsigned char alpha=255);
+    static void DisplayTexture2DCorner(GLuint tex, int w, int h, unsigned char alpha=255);
+
+    static bool SaveTextureToFile(const char* filename, GLuint texID, unsigned w, unsigned h);
+
+	//! Loads texture from GPU mem to an RGBA array (i.e. 32 bits per pixel)
+	static void SaveTextureToArray(unsigned char* data, GLuint texID, unsigned w, unsigned h);
+
+	/***************************************************
+                    OpenGL Matrices
+	***************************************************/
+
+	//! Generates the rotation matrix that transforms a vector in another given one
+	/** WARNING: an OpenGL context must be active!
+		\param sourceVec the original UNIT vector
+		\param destVec the resulting UNIT vector
+		\return 'OpenGL' style 4x4 matrix
+	**/
+	static ccGLMatrix GenerateGLRotationMatrixFromVectors(const float* sourceVec, const float* destVec);
+
+	//! Generates the rotation matrix corresponding to an axis (vector) and an angle
+	/** WARNING: an OpenGL context must be active!
+		\param axis the rotation axis (as a vector)
+		\param angle_deg the rotation angle (in degrees)
+		\return 'OpenGL' style 4x4 matrix
+	**/
+	static ccGLMatrix GenerateGLRotationMatrixFromAxisAndAngle(const float* axis, float angle_deg);
+
+	//! Multiplies tow OpenGL style matrices
+	/** This method uses OpenGL by default.
+		\param A the first matrix
+		\param B the second matrix
+		\param dest the resulting matrix (A*B)
+	**/
+	static void MultGLMatrices(const float* A, const float* B, float* dest);
+
+	//! Transposes an OpenGL style matrix
+	/** \param A a matrix (4*4=16 values)
+		\param dest the resulting matrix (tA)
+	**/
+	static void TransposeGLMatrix(const float* A, float* dest);
+
+    //! Returns a 4x4 'OpenGL' matrix corresponding to a given vue orientation
+    /** \param orientation view orientation
+        \return corresponding GL matrix
+    **/
+    static ccGLMatrix GenerateViewMat(CC_VIEW_ORIENTATION orientation);
+
+	/***************************************************
+                    OpenGL Helpers
+	***************************************************/
+
+	//! Makes all active GL light sources neutral (i.e. 'gray')
+	/** WARNING: an OpenGL context must be active!
+	**/
+	static void MakeLightsNeutral();
+
+	//! Catches last GL error (if any)
+	/** Displays an error message. In debug mode, pauses execution
+		and then exits.
+		\param context name of the method/object that try to catch the error
+		\return true if an error occured, false otherwise
+	**/
+	static bool CatchGLError(const char* context);
+
+
+};
+
+#endif //CC_GL_UTILS_HEADER
---
+++ qCC/shaders/ColorRamp/color_ramp.frag
@@ -0,0 +1,41 @@
+// Color Ramp Shader (CloudCompare - 03/29/2013)
+
+uniform float minSaturation;		//minimum saturation value (between 0 and 1)
+uniform float maxSaturation;		//maximum saturation value (between 0 and 1)
+
+uniform float colormap[256];		//float-packed RGB colors (max: 256)
+uniform int colormapSize;			//colormap size
+uniform float colorGray;			//color for grayed-out points
+
+void main(void)
+{
+	//input: gl_Color
+	// - gl_Color[0] = normalized scalar value
+	// - gl_Color[1] = flag: whether point should be grayed (< 1.0) or not (1.0)
+	// - gl_Color[2] = true lighting value
+	//output: gl_FragColor
+	
+	vec3 unpackedValues = vec3(1.0, 256.0, 65536.0);
+	
+	if (gl_Color[1] == 1.0)
+	{
+		//determine position in current colormap
+		int rampPosi = 0;
+		if (gl_Color[0] <= minSaturation)
+			rampPosi = 0;
+		else if (gl_Color[0] >= maxSaturation)
+			rampPosi = 255;
+		else
+			rampPosi = int((gl_Color[0]-minSaturation)/(maxSaturation-minSaturation)*colormapSize);
+		
+		//unpack the corresponding color
+		unpackedValues = fract(unpackedValues * colormap[rampPosi]);
+	}
+	else //grayed point
+	{
+		unpackedValues = fract(unpackedValues * colorGray);
+	}
+
+	//modulate unpacked color with true lighting value
+	gl_FragColor = vec4( unpackedValues * gl_Color[2], gl_Color[3]);
+}

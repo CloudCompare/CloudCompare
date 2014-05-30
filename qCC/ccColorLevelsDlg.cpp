956ae599b8807c10c0fde8cdf893423b3153aa2e
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2014-05-30 22:30:15 +0200

    Fixing last (partial) commits!

---
+++ qCC/ccColorLevelsDlg.cpp
@@ -0,0 +1,205 @@
+//##########################################################################
+//#                                                                        #
+//#                            CLOUDCOMPARE                                #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU General Public License as published by  #
+//#  the Free Software Foundation; version 2 of the License.               #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#          COPYRIGHT: EDF R&D / TELECOM ParisTech (ENST-TSI)             #
+//#                                                                        #
+//##########################################################################
+
+#include "ccColorLevelsDlg.h"
+
+//local
+#include "ccHistogramWindow.h"
+#include "ccGenericPointCloud.h"
+
+//Qt
+#include <QPushButton>
+
+//system
+#include <string.h>
+#include <assert.h>
+
+//persistent parameters
+static int s_inputLevels[2] = {0,255};
+static int s_outputLevels[2] = {0,255};
+static bool s_outputLevelsEnabled = false;
+
+ccColorLevelsDlg::ccColorLevelsDlg(QWidget* parent, ccGenericPointCloud* pointCloud)
+	: QDialog(parent)
+	, Ui::ColorLevelsDialog()
+	, m_histogram(0)
+	, m_cloud(pointCloud)
+{
+    setupUi(this);
+
+    setWindowFlags(Qt::Tool/*Qt::Dialog | Qt::WindowStaysOnTopHint*/);
+
+	//connect GUI elements
+    connect(channelComboBox,							SIGNAL(currentIndexChanged(int)),	this,	SLOT(onChannelChanged(int)));
+    connect(buttonBox->button(QDialogButtonBox::Apply),	SIGNAL(clicked()),					this,	SLOT(onApply()));
+
+	//create histogram view
+	m_histogram = new ccHistogramWindow(this);
+	{
+		m_histogram->displayParameters().useDefaultParameters = false;
+		m_histogram->displayParameters().useGradientColor = false;
+		//add view
+		histoFrame->setLayout(new QHBoxLayout());
+		histoFrame->layout()->addWidget(m_histogram);		
+	}
+
+	//restore previous parameters
+	minInputSpinBox->setValue(s_inputLevels[0]);
+	maxInputSpinBox->setValue(s_inputLevels[1]);
+	minOutputSpinBox->setValue(s_outputLevels[0]);
+	maxOutputSpinBox->setValue(s_outputLevels[1]);
+	outputLevelsCheckBox->setChecked(s_outputLevelsEnabled);
+
+	updateHistogram();
+}
+
+void ccColorLevelsDlg::updateHistogram()
+{
+	if (m_histogram)
+	{
+		unsigned pointCount = (m_cloud ? m_cloud->size() : 0);
+		if (pointCount == 0)
+		{
+			//nothing to do
+			m_histogram->clear();
+			return;
+		}
+
+		std::vector<unsigned> histoValues[3];
+		try
+		{
+			for (int i=0; i<3; ++i)
+			{
+				if (	channelComboBox->currentIndex() == RGB
+					||	channelComboBox->currentIndex() == i+1 )
+				{
+					histoValues[i].resize(1 << (sizeof(colorType)*8),0);
+				}
+			}
+		}
+		catch(std::bad_alloc)
+		{
+			//not enough memory
+			m_histogram->clear();
+			return;
+		}
+
+		std::vector<unsigned>* histoValuesR = (histoValues[0].empty() ? 0 : histoValues);
+		std::vector<unsigned>* histoValuesG = (histoValues[1].empty() ? 0 : histoValues+1);
+		std::vector<unsigned>* histoValuesB = (histoValues[2].empty() ? 0 : histoValues+2);
+		
+		//test: for now we send all data into the same histogram!
+		if (channelComboBox->currentIndex() == RGB)
+		{
+			histoValuesG = histoValuesR;
+			histoValuesB = histoValuesR;
+		}
+
+		//project points
+		{
+			for (unsigned i=0; i<pointCount; ++i)
+			{
+				const colorType* rgb = m_cloud->getPointColor(i);
+				if (histoValuesR)
+					histoValuesR->at(rgb[0])++;
+				if (histoValuesG)
+					histoValuesG->at(rgb[1])++;
+				if (histoValuesB)
+					histoValuesB->at(rgb[2])++;
+			}
+		}
+
+		static QString names[4] = {"RGB","Red","Green","Blue"};
+		for (int i=0; i<3; ++i)
+		{
+			if (channelComboBox->currentIndex() == RGB || channelComboBox->currentIndex() == i+1)
+			{
+				m_histogram->fromBinArray(histoValues[i],0.0,256.0);
+				m_histogram->setInfoStr(names[channelComboBox->currentIndex()]);
+				break;
+			}
+		}
+		m_histogram->updateGL();
+	}
+}
+
+void ccColorLevelsDlg::onChannelChanged(int channel)
+{
+	updateHistogram();
+}
+
+void ccColorLevelsDlg::onApply()
+{
+	//save parameters
+	s_inputLevels[0] = minInputSpinBox->value();
+	s_inputLevels[1] = maxInputSpinBox->value();
+	s_outputLevels[0] = minOutputSpinBox->value();
+	s_outputLevels[1] = maxOutputSpinBox->value();
+	s_outputLevelsEnabled = outputLevelsCheckBox->isChecked();
+
+	bool applyRGB[3] = {channelComboBox->currentIndex() == RGB || channelComboBox->currentIndex() == RED,
+						channelComboBox->currentIndex() == RGB || channelComboBox->currentIndex() == GREEN,
+						channelComboBox->currentIndex() == RGB || channelComboBox->currentIndex() == BLUE };
+
+	if (	m_cloud
+		&&	(	minInputSpinBox->value() != 0
+			||	maxInputSpinBox->value() != 255
+			||	minOutputSpinBox->value() != 0
+			||	maxOutputSpinBox->value() != 255
+			) )
+	{
+		unsigned pointCount = m_cloud->size();
+		int qIn = s_inputLevels[1] - s_inputLevels[0];
+		int pOut = s_outputLevels[1] - s_outputLevels[0];
+		for (unsigned i=0; i<pointCount; ++i)
+		{
+			const colorType* rgb = m_cloud->getPointColor(i);
+			colorType newRgb[3];
+			for (unsigned c=0; c<3; ++c)
+			{
+				if (applyRGB[c])
+				{
+					double newC = s_outputLevels[0];
+					if (qIn)
+					{
+						double u = (static_cast<double>(rgb[c]) - s_inputLevels[0]) / qIn;
+						newC = s_outputLevels[0] + u * pOut; 
+					}
+					newRgb[c] = static_cast<colorType>(std::max<double>(std::min<double>(newC,MAX_COLOR_COMP),0.0));
+				}
+				else
+				{
+					newRgb[c] = rgb[c];
+				}
+			}
+			memcpy(const_cast<colorType*>(rgb),newRgb,sizeof(colorType)*3);
+		}
+
+		//update display
+		m_cloud->getDisplay()->redraw();
+
+		//update histogram
+		onChannelChanged(channelComboBox->currentIndex());
+	}
+
+	//after applying the filter we reset the boundaries to (0,255)
+	//in case the user clicks multiple times on the "Apply" button!
+	minInputSpinBox->setValue(0);
+	maxInputSpinBox->setValue(255);
+	minOutputSpinBox->setValue(0);
+	maxOutputSpinBox->setValue(255);
+}
---
+++ qCC/ccColorLevelsDlg.h
@@ -0,0 +1,61 @@
+//##########################################################################
+//#                                                                        #
+//#                            CLOUDCOMPARE                                #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU General Public License as published by  #
+//#  the Free Software Foundation; version 2 of the License.               #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#          COPYRIGHT: EDF R&D / TELECOM ParisTech (ENST-TSI)             #
+//#                                                                        #
+//##########################################################################
+
+#ifndef CC_COLOR_LEVELS_DLG_HEADER
+#define CC_COLOR_LEVELS_DLG_HEADER
+
+//Qt
+#include <QColor>
+
+#include <ui_colorLevelsDlg.h>
+
+class ccHistogramWindow;
+class ccGenericPointCloud;
+
+//! Dialog to change the color levels
+class ccColorLevelsDlg : public QDialog, public Ui::ColorLevelsDialog
+{
+    Q_OBJECT
+
+public:
+
+	//! Default constructor
+    ccColorLevelsDlg(QWidget* parent, ccGenericPointCloud* pointCloud);
+
+protected slots:
+
+    void onChannelChanged(int);
+    void onApply();
+
+protected:
+
+	//! Channels
+	enum CHANNELS { RGB = 0, RED = 1, GREEN = 2, BLUE = 3 };
+
+	//! Updates histogram
+	void updateHistogram();
+
+	//! Associated histogram view
+	ccHistogramWindow* m_histogram;
+
+	//! Associated point cloud (color source)
+	ccGenericPointCloud* m_cloud;
+
+
+};
+
+#endif //CC_COLOR_LEVELS_DLG_HEADER

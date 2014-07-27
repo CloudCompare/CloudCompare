24bec137c15d9398ba6e2c7e69cc8c31804e8419
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2014-07-27 09:30:12 +0200

    Filter noise dialog files

---
+++ qCC/ccSORFilterDlg.h
@@ -0,0 +1,32 @@
+//##########################################################################
+//#                                                                        #
+//#                            CLOUDCOMPARE                                #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU General Public License as published by  #
+//#  the Free Software Foundation; version 2 of the License.               #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#          COPYRIGHT: EDF R&D / TELECOM ParisTech (ENST-TSI)             #
+//#                                                                        #
+//##########################################################################
+
+#ifndef CC_SOR_FILTER_DLG_HEADER
+#define CC_SOR_FILTER_DLG_HEADER
+
+#include <ui_sorFilterDlg.h>
+
+//! Dialog to choose which dimension(s) (X, Y or Z) should be exported as SF(s)
+class ccSORFilterDlg : public QDialog, public Ui::SorFilterDialog
+{
+public:
+
+	//! Default constructor
+	ccSORFilterDlg(QWidget* parent = 0);
+};
+
+#endif //CC_SOR_FILTER_DLG_HEADER

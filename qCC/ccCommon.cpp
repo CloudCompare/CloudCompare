a73c343e710e26664b61c61c1d867050e6747499
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2014-10-24 15:21:43 +0200

    Various (final?) updates for V2.6.0

--- qCC/ccCommon.cpp
+++ qCC/ccCommon.cpp
@@ -23,7 +23,7 @@
 #include <CCPlatform.h>
 
 #define CC_VER_NUM "2.6"
-#define CC_SUB_VER "0" //2014-10-17
+#define CC_SUB_VER "0" //2014-10-24
 
 //! Returns current version as a string
 QString ccCommon::GetCCVersion(bool full/*=true*/)

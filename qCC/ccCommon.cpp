ba44247e63a18e1d0800c669f3cb2e03596d13f6
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2015-01-11 15:00:59 +0100

    Updated beta version

--- qCC/ccCommon.cpp
+++ qCC/ccCommon.cpp
@@ -23,7 +23,7 @@
 #include <CCPlatform.h>
 
 #define CC_VER_NUM "2.6"
-#define CC_SUB_VER "1.beta" //2015-01-01
+#define CC_SUB_VER "1.beta" //2015-01-11
 
 //! Returns current version as a string
 QString ccCommon::GetCCVersion(bool full/*=true*/)

82586fb25db18fffdf33d2da32cf58560cacdfad
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2015-08-16 13:34:30 +0200

    The SOR has been integrated into CloudCompare. It can also be called via the command line ('-SOR knn nSigma'). + missing files

---
+++ qCC/ccNoiseFilterDlg.cpp
@@ -0,0 +1,27 @@
+//##########################################################################
+//#                                                                        #
+//#                            CLOUDCOMPARE                                #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU General Public License as published by  #
+//#  the Free Software Foundation; version 2 of the License.               #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#          COPYRIGHT: EDF R&D / TELECOM ParisTech (ENST-TSI)             #
+//#                                                                        #
+//##########################################################################
+
+#include "ccNoiseFilterDlg.h"
+
+ccNoiseFilterDlg::ccNoiseFilterDlg(QWidget* parent/*=0*/)
+	: QDialog(parent)
+	, Ui::NoiseFilterDialog()
+{
+	setupUi(this);
+
+	setWindowFlags(Qt::Tool);
+}
---
+++ qCC/ccNoiseFilterDlg.h
@@ -0,0 +1,32 @@
+//##########################################################################
+//#                                                                        #
+//#                            CLOUDCOMPARE                                #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU General Public License as published by  #
+//#  the Free Software Foundation; version 2 of the License.               #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#          COPYRIGHT: EDF R&D / TELECOM ParisTech (ENST-TSI)             #
+//#                                                                        #
+//##########################################################################
+
+#ifndef CC_NOISE_FILTER_DLG_HEADER
+#define CC_NOISE_FILTER_DLG_HEADER
+
+#include <ui_noiseFilterDlg.h>
+
+//! Dialog for noise filtering (based on the distance to the implicit local surface)
+class ccNoiseFilterDlg : public QDialog, public Ui::NoiseFilterDialog
+{
+public:
+
+	//! Default constructor
+	explicit ccNoiseFilterDlg(QWidget* parent = 0);
+};
+
+#endif //CC_NOISE_FILTER_DLG_HEADER

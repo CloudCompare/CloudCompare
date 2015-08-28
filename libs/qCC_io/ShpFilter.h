268ba6b677d4f93c9ac91390757744a6e2b21e7c
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2015-08-28 11:05:48 +0200

    Copyright update

--- libs/qCC_io/ShpFilter.h
+++ libs/qCC_io/ShpFilter.h
@@ -1,6 +1,6 @@
 //##########################################################################
 //#                                                                        #
-//#                      CLOUDCOMPARE PLUGIN: qBRGM                        #
+//#                            CLOUDCOMPARE                                #
 //#                                                                        #
 //#  This program is free software; you can redistribute it and/or modify  #
 //#  it under the terms of the GNU General Public License as published by  #
@@ -11,7 +11,7 @@
 //#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
 //#  GNU General Public License for more details.                          #
 //#                                                                        #
-//#                           COPYRIGHT: BRGM                              #
+//#          COPYRIGHT: EDF R&D / TELECOM ParisTech (ENST-TSI)             #
 //#                                                                        #
 //##########################################################################
 

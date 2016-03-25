fe00c0885db8941f6a72dc624550c8487cc28fb2
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2016-03-25 08:44:04 +0100

    Copyright fix

--- libs/qCC_glWindow/oculus/ccOculus.h
+++ libs/qCC_glWindow/oculus/ccOculus.h
@@ -11,7 +11,7 @@
 //#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
 //#  GNU General Public License for more details.                          #
 //#                                                                        #
-//#          COPYRIGHT: EDF R&D / TELECOM ParisTech (ENST-TSI)             #
+//#                   COPYRIGHT: CloudCompare project                      #
 //#                                                                        #
 //##########################################################################
 

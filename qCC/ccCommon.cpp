d8d3ca0d4c98783a593f281aa4d5f541114efd7b
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2016-04-22 10:08:06 +0200

    Thrid digit is back for minor/bug fix versions

--- qCC/ccCommon.cpp
+++ qCC/ccCommon.cpp
@@ -23,7 +23,7 @@
 #include <CCPlatform.h>
 
 #define CC_VER_NUM "2"
-#define CC_SUB_VER "7" //2016-04-21
+#define CC_SUB_VER "7.0" //2016-04-21
 
 //! Returns current version as a string
 QString ccCommon::GetCCVersion(bool full/*=true*/)

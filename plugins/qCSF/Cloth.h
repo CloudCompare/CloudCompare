e6db05987cb0f9b63577b474ea4c44373feba486
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2016-09-27 22:37:50 +0200

    Headers updated/added (GPL 2+)

--- plugins/qCSF/Cloth.h
+++ plugins/qCSF/Cloth.h
@@ -1,22 +1,45 @@
-/*
-This source code is about a ground filtering algorithm for airborn LiDAR data
-based on physical process simulations, specifically cloth simulation.
+//#######################################################################################
+//#                                                                                     #
+//#                              CLOUDCOMPARE PLUGIN: qCSF                              #
+//#                                                                                     #
+//#        This program is free software; you can redistribute it and/or modify         #
+//#        it under the terms of the GNU General Public License as published by         #
+//#        the Free Software Foundation; version 2 or later of the License.             #
+//#                                                                                     #
+//#        This program is distributed in the hope that it will be useful,              #
+//#        but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+//#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 #
+//#        GNU General Public License for more details.                                 #
+//#                                                                                     #
+//#        Please cite the following paper, If you use this plugin in your work.        #
+//#                                                                                     #
+//#  Zhang W, Qi J, Wan P, Wang H, Xie D, Wang X, Yan G. An Easy-to-Use Airborne LiDAR  #
+//#  Data Filtering Method Based on Cloth Simulation. Remote Sensing. 2016; 8(6):501.   #
+//#                                                                                     #
+//#                                     Copyright ©                                     #
+//#               RAMM laboratory, School of Geography, Beijing Normal University       #
+//#                               (http://ramm.bnu.edu.cn/)                             #
+//#                                                                                     #
+//#                      Wuming Zhang; Jianbo Qi; Peng Wan; Hongtao Wang                #
+//#                                                                                     #
+//#                      contact us: 2009zwm@gmail.com; wpqjbzwm@126.com                #
+//#                                                                                     #
+//#######################################################################################
 
-This code is based on a Cloth Simulation Tutorial at the cg.alexandra.dk blog.
-Thanks to Jesper Mosegaard (clothTutorial@jespermosegaard.dk)
+//This source code is about a ground filtering algorithm for airborn LiDAR data
+//based on physical process simulations, specifically cloth simulation.
+//
+//This code is based on a Cloth Simulation Tutorial at the cg.alexandra.dk blog.
+//Thanks to Jesper Mosegaard (clothTutorial@jespermosegaard.dk)
+//
+//When applying the cloth simulation to LIDAR point filtering. A lot of features
+//have been added to the original source code, including
+//* configuration file management
+//* point cloud data read/write
+//* point-to-point collsion detection
+//* nearest point search structure from CGAL
+//* addding a terrain class
 
-
-
-When applying the cloth simulation to LIDAR point filtering. A lot of features
-have been added to the original source code, including
-* configuration file management
-* point cloud data read/write
-* point-to-point collsion detection
-* nearest point search structure from CGAL
-* addding a terrain class
-
-
-*/
 //using discrete steps (drop and pull) to approximate the physical process
 //Finding the max height value in nearest N points aroud every particles, as the lowest position where the particles can get.ÔÚÃ¿¸ö²»ÁÏµãÖÜÎ§ÕÒ×îÁÚ½üµÄN¸öµã£¬ÒÔ¸ß³Ì×î´óÖµ×÷ÎªËùÄÜµ½´ïµÄ×îµÍµã¡£
 
--- plugins/qCSF/ccCSFDlg.h
+++ plugins/qCSF/ccCSFDlg.h
@@ -2,7 +2,16 @@
 //#                                                                                     #
 //#                              CLOUDCOMPARE PLUGIN: qCSF                              #
 //#                                                                                     #
-//#  Please cite the following paper, If you use this plugin in your work.              #
+//#        This program is free software; you can redistribute it and/or modify         #
+//#        it under the terms of the GNU General Public License as published by         #
+//#        the Free Software Foundation; version 2 or later of the License.             #
+//#                                                                                     #
+//#        This program is distributed in the hope that it will be useful,              #
+//#        but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+//#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 #
+//#        GNU General Public License for more details.                                 #
+//#                                                                                     #
+//#        Please cite the following paper, If you use this plugin in your work.        #
 //#                                                                                     #
 //#  Zhang W, Qi J, Wan P, Wang H, Xie D, Wang X, Yan G. An Easy-to-Use Airborne LiDAR  #
 //#  Data Filtering Method Based on Cloth Simulation. Remote Sensing. 2016; 8(6):501.   #

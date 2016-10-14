c5ca93920cfd6a5e6beed95e388984742f537396
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2016-10-14 13:59:54 +0200

    Copyright fixed

--- qCC/ccPlaneEditDlg.h
+++ qCC/ccPlaneEditDlg.h
@@ -11,7 +11,7 @@
 //#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
 //#  GNU General Public License for more details.                          #
 //#                                                                        #
-//#                  COPYRIGHT: Daniel Girardeau-Montaut                   #
+//#                       COPYRIGHT: SAGE INGENIERIE                       #
 //#                                                                        #
 //##########################################################################
 

53ba64fd468870609ba0d0f4b5bdd864b5a46fcb
Romain Janvier <romain.janvier@hotmail.fr>
2016-12-07 22:18:16 +0100

    [Debian] working on the release script

--- debian/release.sh
+++ debian/release.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+#oses = [trusty etc...]
+CURR_VERSION=$(git describe --always --tags --long | sed 's/v//g')
+CURR_HASH=${CURR_VERSION##*-g}
+PREV_VERSION=$(dpkg-parsechangelog --show-field Version)
+PREV_HASH=${PREV_VERSION##*-}
+echo $CURR_HASH
+#collect all [Debian] commits ]PREV_HASH, CUR_HASH]
+#git shortlog --grep='\[Debian\]' --oneline

d2c8c621c6bba73d8ff6ee73e8b999a2b97cde72
Romain Janvier <romain.janvier@hotmail.fr>
2016-12-09 00:00:29 +0100

    [Debian] working on release script [NoChangelog]

--- debian/release.sh
+++ debian/release.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
-#oses = [trusty, xenial, wheezy etc...]
-#TODO a no commit build after failure (but ths should never fail ;)
-PACKAGE_NAME="cloudcompare"
+#TODO a 'no commit build' to recover after failure (but ths should never fail ;)
+#TODO an increment PACKAGE_VERSION build
+
+PACKAGE_VERSION=0
+DIST="unstable"
+ID="Romain Janvier <romain.janvier@univ-orleans.fr>"
 TIMESTAMP=$(date -R)
-CURR_VERSION=$(git describe --always --tags --long | sed -e 's/v//g' -e 's/-/+/g') #TODO: only first occurence
+CURR_UPSTREAM_VERSION=$(git describe --always --tags --long | sed -e 's/v//g' -e 's/-/+/g')
 CURR_HASH=${CURR_VERSION##*+g}
-#PREV_VERSION=$(dpkg-parsechangelog --show-field Version | sed -e 's/-/+/g')
+#PREV_UPSTREAM_VERSION=$(dpkg-parsechangelog --show-field Version | sed -e 's/-/+/g')
 #PREV_HASH=${PREV_VERSION##*+g}
 #echo $CURR_VERSION
-#PREV_HASH=1221f69e
+PREV_HASH=5018adb
 
 #collect all [Debian] commits and no [NoChangelog] ]PREV_HASH, CUR_HASH]
 #finding the children of a commit is too difficult with git, so we will trim [PREV_HASH] by a [NoChangeLog]
-CHANGESET=$(git shortlog --grep='\[Debian\]' -w76,2,2 $PREV_HASH..$CURR_HASH | sed -e 's/\[Debian\]/\-/g' -e '/\[NoChangelog\]/d' -e 's/ ([0-9]*)//g')
-echo "$CHANGESET"
+HEADER="cloudcompare ($CURR_UPSTREAM_VERSION-$PACKAGE_VERSION) $DIST; urgency=low"
+CHANGESET=$(git log --grep='\[Debian\]' --oneline $PREV_HASH..$CURR_HASH | cut -c 9- | sed -e 's/^/  /' -e 's/\[Debian\]/\-/g' -e '/\[NoChangelog\]/d' -e 's/ ([0-9]*)//g')
+FOOTER=" -- $ID  $TIMESTAMP"
+CHANGELOG_ENTRY="$HEADER\n\n$CHANGESET\n\n$FOOTER\n"
 
-#Commit: git commit -am "[Debian] Update changelog for $CURR_VERSION [NoChangelog]"
+echo "$CHANGELOG_ENTRY"
+#git commit -am "[Debian] Update changelog for $CURR_VERSION [NoChangelog]"
 
-#creating a new branch for each OS, in each branch
+#oses = [trusty, xenial, wheezy etc...]
+#create a new branch for each OS, in each branch
 	#looking for specific rules etc... and make substitution in a changelog
 	#commit (--amend ?)
-	#Update chroot
+	#Update the chroots (check for chroot ?)
 	#build (-uc -us ?)
 	#Upload to bintray
 

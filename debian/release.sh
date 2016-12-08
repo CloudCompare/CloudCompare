42faccc3a4da624ec22f1319d8d9bdf8c30b8d6d
Romain Janvier <romain.janvier@univ-orleans.fr>
2016-12-08 13:45:20 +0100

    [Debian] Working a bit on the release script [NoChangelog]

--- debian/release.sh
+++ debian/release.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 #oses = [trusty etc...]
+
 CURR_VERSION=$(git describe --always --tags --long | sed 's/v//g')
 CURR_HASH=${CURR_VERSION##*-g}
 PREV_VERSION=$(dpkg-parsechangelog --show-field Version)
 PREV_HASH=${PREV_VERSION##*-}
-echo $CURR_HASH
-#collect all [Debian] commits ]PREV_HASH, CUR_HASH]
-#git shortlog --grep='\[Debian\]' --oneline
+#collect all [Debian] commits and no [NoChangelog] ]PREV_HASH, CUR_HASH]
+#finding the children of a commit is too difficult with git, so we will trim [PREV_HASH] by a [NoChangeLog]
+CHANGESET=$(git shortlog --grep='\[Debian\]' --oneline $PREV_HASH..$CURR_HASH | sed -e 's/\[Debian\]//g' -e '/\[NoChangelog\]/d' -e 's/ ([0-9]*)//g')
+echo $CHANGESET
+#Commit: "Update changelog for $CURR_VERSION [NoChangelog]"
+#creating a new branch for each OS, in each branch
+	#looking for specific rules etc... and make substitution in a changelog
+	#commit (--amend ?)
+	#Update chroot
+	#build (-uc -us ?)
+	#Upload to bintray
+
+#TODO chroot cache + chroot no credentials

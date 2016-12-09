29c40944f0b5a64308cee6da919b84f01088bfec
Romain Janvier <romain.janvier@univ-orleans.fr>
2016-12-09 16:33:59 +0100

    [Debian] Working a bit on the release script [NoChangelog]

--- debian/release.sh
+++ debian/release.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
-#TODO a 'no commit build' to recover after failure (but ths should never fail ;)
-#TODO an increment PACKAGE_VERSION build
+# TODO a 'no commit build' to recover after failure (but this should never fail ;)
+# TODO an increment PACKAGE_VERSION build
 
 PACKAGE_VERSION=0
 DIST="unstable"
 ID="Romain Janvier <romain.janvier@univ-orleans.fr>"
 TIMESTAMP=$(date -R)
 CURR_UPSTREAM_VERSION=$(git describe --always --tags --long | sed -e 's/v//g' -e 's/-/+/g')
-CURR_HASH=${CURR_VERSION##*+g}
-#PREV_UPSTREAM_VERSION=$(dpkg-parsechangelog --show-field Version | sed -e 's/-/+/g')
-#PREV_HASH=${PREV_VERSION##*+g}
-#echo $CURR_VERSION
-PREV_HASH=5018adb
+CURR_HASH=${CURR_UPSTREAM_VERSION##*+g}
+PREV_UPSTREAM_VERSION=$(dpkg-parsechangelog --show-field Version)
+PREV_HASH=${PREV_UPSTREAM_VERSION%%-*}; PREV_HASH=${PREV_HASH##*+g}
 
-#collect all [Debian] commits and no [NoChangelog] ]PREV_HASH, CUR_HASH]
-#finding the children of a commit is too difficult with git, so we will trim [PREV_HASH] by a [NoChangeLog]
+# Collect all [Debian] commits and no [NoChangelog] ]PREV_HASH, CUR_HASH]
+# Find the children of a commit is too difficult with git, so we will trim [PREV_HASH] by a [NoChangeLog]
 HEADER="cloudcompare ($CURR_UPSTREAM_VERSION-$PACKAGE_VERSION) $DIST; urgency=low"
 CHANGESET=$(git log --grep='\[Debian\]' --oneline $PREV_HASH..$CURR_HASH | cut -c 9- | sed -e 's/^/  /' -e 's/\[Debian\]/\-/g' -e '/\[NoChangelog\]/d' -e 's/ ([0-9]*)//g')
 FOOTER=" -- $ID  $TIMESTAMP"
 CHANGELOG_ENTRY="$HEADER\n\n$CHANGESET\n\n$FOOTER\n"
 
-echo "$CHANGELOG_ENTRY"
+echo "$CHANGELOG_ENTRY\n$(cat debian/changelog)" > debian/changelog
 #git commit -am "[Debian] Update changelog for $CURR_VERSION [NoChangelog]"
+#gbp buildpackage --git-dist=xenial --git-arch=amd64 --git-pbuilder --git-debian-branch=Debian --git-submodules
 
-#oses = [trusty, xenial, wheezy etc...]
+#distrib = [trusty, xenial, wheezy etc...]
+#arch = [amd64, i386]
+
 #create a new branch for each OS, in each branch
 	#looking for specific rules etc... and make substitution in a changelog
 	#commit (--amend ?)

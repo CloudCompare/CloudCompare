23e36425d1768fb1083782ab23186368d4082feb
Jälv <thomas.montaigu@laposte.net>
2017-07-01 15:55:46 +0200

    Add PDALSupport.cmake

---
+++ contrib/PDALSupport.cmake
@@ -0,0 +1,27 @@
+# pdal
+OPTION( OPTION_PDAL_LAS "Build with PDAL to support las format" OFF)
+if ( ${OPTION_PDAL_LAS} )
+    find_package(PDAL 1.0.0 REQUIRED CONFIG)
+    if ( NOT PDAL_FOUND )
+        message( SEND_ERROR "PDAL package not found!")
+    else()
+        include_directories(${PDAL_INCLUDE_DIRS})
+    endif()
+endif()
+
+
+# Link project with PDAL library
+function( target_link_PDAL ) # 2 arguments: ARGV0 = project name / ARGV1 = base lib export folder (optional)
+    if( ${OPTION_PDAL_LAS} )
+        if( PDAL_FOUND )
+            add_definitions(${PDAL_DEFINITIONS})
+            target_link_libraries(${ARGV0} ${PDAL_LIBRARIES})
+            set_property( TARGET ${ARGV0} APPEND PROPERTY COMPILE_DEFINITIONS CC_LAS_SUPPORT )
+            #Win32 copy dll?!
+
+        else()
+            message( SEND_ERROR "PDAL package not found: can't link" )
+        endif()
+    endif()
+endfunction()
+

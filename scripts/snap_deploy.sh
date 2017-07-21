9cb0e002e75e3b76bb24454d9eaf38aef8044971
Andy Maloney <asmaloney@gmail.com>
2017-07-21 11:20:41 -0400

    Attempt Travis deployment fix #2

--- scripts/snap_deploy.sh
+++ scripts/snap_deploy.sh
@@ -2,6 +2,6 @@
 
 WORKING_DIR="$1"
 
-docker run -v "${WORKING_DIR}":"${WORKING_DIR}" -t ubuntu:xenial sh -c "apt update -qq && apt
-  install snapcraft -y && cd ${WORKING_DIR} && snapcraft && snapcraft push *.snap --release
+docker run -v "${WORKING_DIR}":"${WORKING_DIR}" -t ubuntu:xenial sh -c "apt-get -qq update && apt-get -qq -y
+  install snapcraft && cd ${WORKING_DIR} && snapcraft && snapcraft push *.snap --release
   edge"

bf878da58bfcb6afa253b77716080f5b51ec8f1b
Andy Maloney <asmaloney@gmail.com>
2017-07-22 07:35:16 -0400

    Attempt Travis deployment fix #6
    (This is getting tedious)

--- scripts/linux/snap_deploy.sh
+++ scripts/linux/snap_deploy.sh
@@ -2,9 +2,9 @@
 
 WORKING_DIR="$1"
 
-docker run \
+docker run -t \
   -v "${WORKING_DIR}":"${WORKING_DIR}" \
-  -t ubuntu:xenial \
   -e LC_ALL="C.UTF-8" \
   -e LANG="C.UTF-8" \
+  ubuntu:xenial \
   sh -c "apt-get -qq update && apt-get -y install snapcraft && cd ${WORKING_DIR} && snapcraft && snapcraft push *.snap --release edge"

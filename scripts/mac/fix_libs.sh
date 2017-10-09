8b73baac2bdb0baeba30d59419e515a78c67d679
Andy Maloney <asmaloney@gmail.com>
2017-10-09 14:33:09 -0400

    {mac} Update version of CGAL in dynamic lib packaging script

--- scripts/mac/fix_libs.sh
+++ scripts/mac/fix_libs.sh
@@ -8,9 +8,9 @@ FRAMEWORK_DIR=$1
 # If you are trying to package things yourself for general deployment, you'll have to change this path.
 HOMEBREW_PATH_PREFIX=${HOME}/dev
 
-if [ -f "${FRAMEWORK_DIR}/libCGAL_Core.12.dylib" ]; then
-   echo "  fixing: ${FRAMEWORK_DIR}/libCGAL_Core.12.dylib"
-   install_name_tool -change "${HOMEBREW_PATH_PREFIX}/lib/libCGAL.12.dylib" "@executable_path/../Frameworks/libCGAL.12.dylib" "${FRAMEWORK_DIR}/libCGAL_Core.12.dylib"
+if [ -f "${FRAMEWORK_DIR}/libCGAL_Core.13.dylib" ]; then
+   echo "  fixing: ${FRAMEWORK_DIR}/libCGAL_Core.13.dylib"
+   install_name_tool -change "${HOMEBREW_PATH_PREFIX}/lib/libCGAL.13.dylib" "@executable_path/../Frameworks/libCGAL.13.dylib" "${FRAMEWORK_DIR}/libCGAL_Core.13.dylib"
 fi
 
 echo "  fixing: ${FRAMEWORK_DIR}/libboost_chrono-mt.dylib"

b4daf045d2a402d1c0933a739804906f3d7a6e49
Andy Maloney <asmaloney@gmail.com>
2018-03-16 12:48:16 -0400

    {cmake} Give CC's poor cousin some attention
    - move qCC files into an include so they are easier to manage
    - remove unused ccMouse3DContextMenu.[h,cpp] from the list
    - use the new add_subdirectory for devices

---
+++ ccViewer/CMakeIncludeQCCFiles.cmake
@@ -0,0 +1,28 @@
+# Include specific capabilities from qCC
+
+list( APPEND header_list
+	../qCC/ccCameraParamEditDlg.h
+	../qCC/ccDisplayOptionsDlg.h
+	../qCC/ccOptions.h
+	../qCC/ccOverlayDialog.h
+	../qCC/ccPickingHub.h
+	../qCC/ccPickOneElementDlg.h
+	../qCC/ccStereoModeDlg.h
+)
+
+list( APPEND source_list
+	../qCC/ccCameraParamEditDlg.cpp
+	../qCC/ccDisplayOptionsDlg.cpp
+	../qCC/ccOptions.cpp
+	../qCC/ccOverlayDialog.cpp
+	../qCC/ccPickingHub.cpp
+	../qCC/ccPickOneElementDlg.cpp
+	../qCC/ccStereoModeDlg.cpp
+)
+
+list( APPEND ui_list
+	../qCC/ui_templates/cameraParamDlg.ui
+	../qCC/ui_templates/displayOptionsDlg.ui
+	../qCC/ui_templates/pickOneElementDlg.ui
+	../qCC/ui_templates/stereoModeDlg.ui
+)

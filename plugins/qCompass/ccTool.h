bba7ab0cf4272ecb03232b3bf06ec21182c21210
Andy Maloney <asmaloney@gmail.com>
2018-03-28 09:21:57 -0400

    {qCompass} Cleanup ccTool interface
    - properly initialize members in constructor
    - constructor should be protected since only derived classes should call it
    - initializeTool() doesn't need to be virtual
    - remove extra semicolons

--- plugins/qCompass/ccTool.h
+++ plugins/qCompass/ccTool.h
@@ -30,33 +30,30 @@ class ccTool
 {
 public:
 
-	ccTool()
+	virtual ~ccTool()
 	{
 	}
-
-	~ccTool()
-	{
-	}
-
-	virtual void initializeTool(ccMainAppInterface* app)
+	
+	void initializeTool(ccMainAppInterface* app)
 	{
 		m_app = app; //store copy of app
 		m_window = m_app->getActiveGLWindow();
 	}
+
 	//called when the tool is set to active (for initialization)
-	virtual void toolActivated() { };
+	virtual void toolActivated() { }
 
 	//called when the tool is set to disactive (for cleanup)
-	virtual void toolDisactivated() { };
+	virtual void toolDisactivated() { }
 
 	//called when a point in a point cloud gets picked while this tool is active
-	virtual void pointPicked(ccHObject* insertPoint, unsigned itemIdx, ccHObject* pickedObject, const CCVector3& P) { };
+	virtual void pointPicked(ccHObject* insertPoint, unsigned itemIdx, ccHObject* pickedObject, const CCVector3& P) { }
 
 	//called when a point in a point cloud gets picked while this tool is active
-	virtual void pointPicked(ccHObject* insertPoint, unsigned itemIdx, ccPointCloud* cloud, const CCVector3& P) { };
+	virtual void pointPicked(ccHObject* insertPoint, unsigned itemIdx, ccPointCloud* cloud, const CCVector3& P) { }
 
 	//called when the selection is changed while this tool is active
-	virtual void onNewSelection(const ccHObject::Container& selectedEntities) { };
+	virtual void onNewSelection(const ccHObject::Container& selectedEntities) { }
 
 	//called when "Return" or "Space" is pressed, or the "Accept Button" is clicked
 	virtual void accept() { }
@@ -69,7 +66,15 @@ public:
 
 	//called when the undo button is clicked
 	virtual void undo()	{ }
+
 protected:
+	
+	ccTool() :
+		m_app( nullptr )
+	  , m_window( nullptr )
+	{
+	}
+
 	//link to the main plugin interface
 	ccMainAppInterface* m_app;
 

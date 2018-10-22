f88ef1b90a1164f168c4f5e231e4fd777b3a7607
Andy Maloney <asmaloney@gmail.com>
2018-10-22 12:56:42 -0400

    Add a new "common" directory to put code that is common between the viewer and the main app
    Create a common application base class since the two application classes are almost the same (will be used in the next commit)

---
+++ common/ccApplicationBase.h
@@ -0,0 +1,43 @@
+#ifndef CCAPPLICATIONBASE_H
+#define CCAPPLICATIONBASE_H
+
+//##########################################################################
+//#                                                                        #
+//#                              CLOUDCOMPARE                              #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU General Public License as published by  #
+//#  the Free Software Foundation; version 2 or later of the License.      #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#          COPYRIGHT: CloudCompare project                               #
+//#                                                                        #
+//##########################################################################
+
+#include <QApplication>
+
+
+//! Mimic Qt's qApp for easy access to the application instance
+#define ccApp (static_cast<ccApplicationBase *>( QCoreApplication::instance() ))
+
+class ccApplicationBase : public QApplication
+{
+public:
+	//! This must be called before instantiating the application class so it
+	//! can setup OpenGL first.
+	static void	init();
+
+	ccApplicationBase( int &argc, char **argv, const QString &version );
+
+	QString versionStr() const;
+	QString versionLongStr( bool includeOS ) const;
+
+private:
+	const QString c_VersionStr;
+};
+
+#endif

d891e45652bbd4aa63b0b9b023f8352dd13cc0e5
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2018-12-03 23:07:41 +0100

    Moving to '2.10.beta'

--- qCC/ccApplication.cpp
+++ qCC/ccApplication.cpp
@@ -28,7 +28,7 @@
 #include "mainwindow.h"
 
 ccApplication::ccApplication(int &argc, char **argv)
-	: ccApplicationBase( argc, argv, QStringLiteral( "2.10-alpha" ) )
+	: ccApplicationBase( argc, argv, QStringLiteral( "2.10-beta" ) )
 {
 	setApplicationName( "CloudCompare" );
 	

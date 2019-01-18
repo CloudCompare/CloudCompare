79625880fe71f884d28f4aede8c6e11b0839c2b4
Andy Maloney <asmaloney@gmail.com>
2019-01-18 08:58:40 -0500

    {cmake} Replace setting individual COMPILE_DEFINITIONS with setting it for all configs

--- contrib/3DXSupport.cmake
+++ contrib/3DXSupport.cmake
@@ -27,19 +27,10 @@ function( target_link_3DXWARE ) # 1 argument: ARGV0 = project name
 				target_link_libraries( ${ARGV0} optimized ${3DXWARE_LIB_DIR}/siapp.lib ${3DXWARE_LIB_DIR}/spwmath.lib)
 			
 				if ( CMAKE_CONFIGURATION_TYPES )
-				
 					target_link_libraries( ${ARGV0} debug ${3DXWARE_LIB_DIR}/siapp.lib ${3DXWARE_LIB_DIR}/spwmathD.lib )
-
-					#Anytime we use COMPILE_DEFINITIONS_XXX we must define this policy!
-					#(and setting it outside of the function/file doesn't seem to work...)
-					cmake_policy(SET CMP0043 OLD)
-
-					set_property( TARGET ${ARGV0} APPEND PROPERTY COMPILE_DEFINITIONS_RELEASE CC_3DXWARE_SUPPORT )
-					set_property( TARGET ${ARGV0} APPEND PROPERTY COMPILE_DEFINITIONS_RELWITHDEBINFO CC_3DXWARE_SUPPORT )
-					set_property( TARGET ${ARGV0} APPEND PROPERTY COMPILE_DEFINITIONS_DEBUG CC_3DXWARE_SUPPORT )
-				else()
-					set_property( TARGET ${ARGV0} APPEND PROPERTY COMPILE_DEFINITIONS CC_3DXWARE_SUPPORT )
 				endif()
+				
+				set_property( TARGET ${ARGV0} APPEND PROPERTY COMPILE_DEFINITIONS CC_3DXWARE_SUPPORT )
 			else()
 				message( SEND_ERROR "3DXWARE libraries folder is not specified (3DXWARE_LIB_DIR)" )
 			endif()

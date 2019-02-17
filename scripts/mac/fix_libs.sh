1c4c5c25ebd237fc57552bc89223ed509bf5aa52
Andy Maloney <asmaloney@gmail.com>
2019-02-17 10:16:34 -0500

    {macOS} Statically link ffmpeg
    Homebrew changed their policies of allowing options in their builds, so ffmpeg now has about 40 libraries it pulls in which makes it useless for shipping anything.
    
    Normally I would wait for 2.11 to make this change, but it's too difficult to go back-and forth between versions.
    
    I now build ffmpeg myself using the following options and link to it statically:
    
      ./configure \
          --enable-gpl --enable-version3 \
          --enable-pthreads \
          --enable-hardcoded-tables \
          --disable-all \
          --disable-doc --disable-network  --disable-sdl2 \
          --enable-avcodec --enable-avformat --enable-swscale \
          --enable-encoder=libx264 \
          --enable-protocol=file \
          --enable-muxer=mp4 \
          --enable-libx264

--- scripts/mac/fix_libs.sh
+++ scripts/mac/fix_libs.sh
@@ -20,37 +20,3 @@ install_name_tool -change "@loader_path/libboost_system-mt.dylib" "@executable_p
 
 echo "  fixing: ${FRAMEWORK_DIR}/libboost_thread-mt.dylib"
 install_name_tool -change "@loader_path/libboost_system-mt.dylib" "@executable_path/../Frameworks/libboost_system-mt.dylib" "${FRAMEWORK_DIR}/libboost_thread-mt.dylib"
-
-## FFMPEG from homebrew is problematic since it puts the "Cellar" paths directly in the libs
-## Again, these paths need to be modified if you are shipping the version of CC you are installing...
-
-if test -n "$(find ${FRAMEWORK_DIR} -maxdepth 1 -name 'libavcodec.*.dylib*' -print -quit)"; then
-  FFMPEG_VERSION=$(ffmpeg -version | head -n1 | sed "s/^.*version \([0-9.]*\) Copyright.*/\1/")
-  FFMPEG_DIR="${HOMEBREW_PATH_PREFIX}/Cellar/ffmpeg/${FFMPEG_VERSION}/lib"
-
-  if [ ! -d "$FFMPEG_DIR" ]; then
-    echo "  error: could not find FFMPEG directory: ${FFMPEG_DIR} - check versions in fix_libs.sh"
-  else
-    AV_CODEC="libavcodec.58.dylib"
-    AV_FORMAT="libavformat.58.dylib"
-    AV_UTIL="libavutil.56.dylib"
-
-    SW_RESAMPLE="libswresample.3.dylib"
-    SW_SCALE="libswscale.5.dylib"
-
-    echo "  fixing: ${FRAMEWORK_DIR}/${AV_CODEC}"
-    install_name_tool -change "${FFMPEG_DIR}/${AV_UTIL}" "@executable_path/../Frameworks/${AV_UTIL}" "${FRAMEWORK_DIR}/${AV_CODEC}"
-    install_name_tool -change "${FFMPEG_DIR}/${SW_RESAMPLE}" "@executable_path/../Frameworks/${SW_RESAMPLE}" "${FRAMEWORK_DIR}/${AV_CODEC}"
-
-    echo "  fixing: ${FRAMEWORK_DIR}/${AV_FORMAT}"
-    install_name_tool -change "${FFMPEG_DIR}/${AV_CODEC}" "@executable_path/../Frameworks/${AV_CODEC}" "${FRAMEWORK_DIR}/${AV_FORMAT}"
-    install_name_tool -change "${FFMPEG_DIR}/${AV_UTIL}" "@executable_path/../Frameworks/${AV_UTIL}" "${FRAMEWORK_DIR}/${AV_FORMAT}"
-    install_name_tool -change "${FFMPEG_DIR}/${SW_RESAMPLE}" "@executable_path/../Frameworks/${SW_RESAMPLE}" "${FRAMEWORK_DIR}/${AV_FORMAT}"
-
-    echo "  fixing: ${FRAMEWORK_DIR}/${SW_RESAMPLE}"
-    install_name_tool -change "${FFMPEG_DIR}/${AV_UTIL}" "@executable_path/../Frameworks/${AV_UTIL}" "${FRAMEWORK_DIR}/${SW_RESAMPLE}"
-
-    echo "  fixing: ${FRAMEWORK_DIR}/${SW_SCALE}"
-    install_name_tool -change "${FFMPEG_DIR}/${AV_UTIL}" "@executable_path/../Frameworks/${AV_UTIL}" "${FRAMEWORK_DIR}/${SW_SCALE}"
-  fi
-fi

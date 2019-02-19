a69c4af1379d9ebd45a0e69930115c2819a0e0d3
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2019-02-19 22:28:36 +0100

    Better version of the script

--- scripts/windows/updateTranslations.bat
+++ scripts/windows/updateTranslations.bat
@@ -1,8 +1,12 @@
 set QT_DIR="<path to Qt installation bin folder>"
 
+set SRC_DIRS=common qCC plugins/core libs/CCFbo libs/qCC_db libs/qCC_glWindow libs/qCC_io
+
 echo "Updating translation files"
 
 %call lupdate
-%QT_DIR%\lupdate.exe ..\..\qCC -no-obsolete -ts ..\..\qCC\translations\CloudCompare_fr.ts
-%QT_DIR%\lupdate.exe ..\..\qCC -no-obsolete -ts ..\..\qCC\translations\CloudCompare_pt.ts
-%QT_DIR%\lupdate.exe ..\..\qCC -no-obsolete -ts ..\..\qCC\translations\CloudCompare_ru.ts
+cd ..\..
+%QT_DIR%\lupdate.exe %SRC_DIRS% -no-obsolete -ts qCC\translations\CloudCompare_fr.ts
+%QT_DIR%\lupdate.exe %SRC_DIRS% -no-obsolete -ts qCC\translations\CloudCompare_pt.ts
+%QT_DIR%\lupdate.exe %SRC_DIRS% -no-obsolete -ts qCC\translations\CloudCompare_ru.ts
+cd scripts\windows

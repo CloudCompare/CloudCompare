57f0c58af3392aca73176833a30e003a626bf678
Chris S Brown <Chris.S.Brown@Boeing.com>
2019-11-12 10:51:18 -0800

    ccComparison QT5 style connects Also moved the processEvents and compute distances into a singleshot timer lambda to remove them from the constructor

--- qCC/ccComparisonDlg.cpp
+++ qCC/ccComparisonDlg.cpp
@@ -129,24 +129,20 @@ ccComparisonDlg::ccComparisonDlg(	ccHObject* compEntity,
 		filterVisibilityCheckBox->setEnabled(m_refCloud && m_refCloud->isA(CC_TYPES::POINT_CLOUD) && static_cast<ccPointCloud*>(m_refCloud)->hasSensor());
 	}
 
-	connect(cancelButton,			&QPushButton::clicked,					this,	ccComparisonDlg::cancelAndExit);
-	connect(okButton,				&QPushButton::clicked,					this,	ccComparisonDlg::applyAndExit);
-	connect(computeButton,			&QPushButton::clicked,					this,	ccComparisonDlg::computeDistances);
-	connect(histoButton,			&QPushButton::clicked,					this,	ccComparisonDlg::showHisto);
-	connect(maxDistCheckBox,		&QCheckBox::toggled,					this,	ccComparisonDlg::maxDistUpdated);
-	connect(localModelComboBox, static_cast<void (QComboBox::*)(int)> (&QComboBox::currentIndexChanged),		this,	ccComparisonDlg::locaModelChanged);
-	connect(maxSearchDistSpinBox, static_cast<void (QDoubleSpinBox::*)(double)> (&QDoubleSpinBox::valueChanged),this,	ccComparisonDlg::maxDistUpdated);
+	connect(cancelButton,			&QPushButton::clicked,					this,	&ccComparisonDlg::cancelAndExit);
+	connect(okButton,				&QPushButton::clicked,					this,	&ccComparisonDlg::applyAndExit);
+	connect(computeButton,			&QPushButton::clicked,					this,	&ccComparisonDlg::computeDistances);
+	connect(histoButton,			&QPushButton::clicked,					this,	&ccComparisonDlg::showHisto);
+	connect(maxDistCheckBox,		&QCheckBox::toggled,					this,	&ccComparisonDlg::maxDistUpdated);
+	connect(localModelComboBox, static_cast<void (QComboBox::*)(int)> (&QComboBox::currentIndexChanged),		this,	&ccComparisonDlg::locaModelChanged);
+	connect(maxSearchDistSpinBox, static_cast<void (QDoubleSpinBox::*)(double)> (&QDoubleSpinBox::valueChanged),this,	&ccComparisonDlg::maxDistUpdated);
 
 	//be sure to show the dialog before computing the approx distances
 	//(otherwise the progress bars appear anywhere!)
-	if (!m_noDisplay)
-	{
-		show();
-		QCoreApplication::processEvents();
-	}
-
-	//compute approximate results and unlock GUI
-	computeApproxDistances();
+	QTimer::singleShot(0, this, [=]() { //singleshot 0 time will fire once after all events in the queue are processed
+					QCoreApplication::processEvents();	
+					computeApproxDistances();//compute approximate results and unlock GUI
+					});
 }
 
 ccComparisonDlg::~ccComparisonDlg()

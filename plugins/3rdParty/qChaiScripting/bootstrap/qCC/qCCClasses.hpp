83c845a6457db0eae5c3323bf15cc7b474ab7402
Chris S Brown <Chris.S.Brown@Boeing.com>
2020-01-29 17:27:01 -0800

    Added ccCommandLineInterface to chaiscript

--- plugins/3rdParty/qChaiScripting/bootstrap/qCC/qCCClasses.hpp
+++ plugins/3rdParty/qChaiScripting/bootstrap/qCC/qCCClasses.hpp
@@ -21,7 +21,9 @@
 
 #include <chaiscript/chaiscript.hpp>
 #include <chaiscript/utility/utility.hpp>
+
 #include <ccMainAppInterface.h>
+#include <ccCommandLineInterface.h>
 #include <ccHObject.h>
 #include <QtGui>
 #include <QString>
@@ -29,6 +31,7 @@
 #include <ccOverlayDialog.h>
 #include <ccColorScalesManager.h>
 #include <ccPickingHub.h>
+
 
 namespace chaiscript
 {
@@ -95,14 +98,123 @@ namespace chaiscript
 				m->add(fun(&ccMainAppInterface::setGlobalZoom), "setGlobalZoom");
 				m->add(fun(&ccMainAppInterface::increasePointSize), "increasePointSize");
 				m->add(fun(&ccMainAppInterface::decreasePointSize), "decreasePointSize");
+				m->add(chaiscript::user_type<ccMainAppInterface::ccHObjectContext>(), "ccHObjectContext");
+				m->add(fun(&ccMainAppInterface::ccHObjectContext::parent), "parent");
+				m->add(fun(&ccMainAppInterface::ccHObjectContext::childFlags), "childFlags");
+				m->add(fun(&ccMainAppInterface::ccHObjectContext::parentFlags), "parentFlags");
 				return m;
 			}
 
+			ModulePtr bs_ccCommandLineInterface(ModulePtr m = std::make_shared<Module>())
+			{
+				m->add(chaiscript::user_type<CLEntityDesc>(), "CLEntityDesc");
+				m->add(fun(&CLEntityDesc::basename), "basename");
+				m->add(fun(&CLEntityDesc::path), "path");
+				m->add(fun(&CLEntityDesc::indexInFile), "indexInFile");
+				m->add(fun(static_cast<ccHObject*(CLEntityDesc::*)()>(&CLEntityDesc::getEntity)), "getEntity");
+				m->add(fun(static_cast<const ccHObject*(CLEntityDesc::*)()const>(&CLEntityDesc::getEntity)), "getEntity");
+
+				m->add(chaiscript::user_type<CLGroupDesc>(), "CLGroupDesc");
+				m->add(chaiscript::constructor<CLGroupDesc(ccHObject*, const QString&, const QString&)>(), "CLGroupDesc");
+				m->add(fun(&CLGroupDesc::groupEntity), "groupEntity");
+				m->add(fun(static_cast<ccHObject * (CLGroupDesc::*)()>(&CLGroupDesc::getEntity)), "getEntity");
+				m->add(fun(static_cast<const ccHObject * (CLGroupDesc::*)()const>(&CLGroupDesc::getEntity)), "getEntity");
+
+
+				m->add(chaiscript::user_type<CLCloudDesc>(), "CLCloudDesc");
+				m->add(chaiscript::constructor<CLCloudDesc()>(), "CLCloudDesc");
+				m->add(chaiscript::constructor<CLCloudDesc(ccPointCloud*, const QString&, int)>(), "CLCloudDesc");
+				m->add(chaiscript::constructor<CLCloudDesc(ccPointCloud*, const QString&, const QString&, int)>(), "CLCloudDesc");
+				m->add(fun(&CLCloudDesc::pc), "pc");
+				m->add(fun(static_cast<ccHObject * (CLCloudDesc::*)()>(&CLCloudDesc::getEntity)), "getEntity");
+				m->add(fun(static_cast<const ccHObject * (CLCloudDesc::*)()const>(&CLCloudDesc::getEntity)), "getEntity");
+
+
+				m->add(chaiscript::user_type<CLMeshDesc>(), "CLMeshDesc");
+				m->add(chaiscript::constructor<CLMeshDesc()>(), "CLMeshDesc");
+				m->add(chaiscript::constructor<CLMeshDesc(ccGenericMesh*, const QString&, int)>(), "CLMeshDesc");
+				m->add(chaiscript::constructor<CLMeshDesc(ccGenericMesh*, const QString&, const QString&, int)>(), "CLMeshDesc");
+				m->add(fun(&CLMeshDesc::mesh), "mesh");
+				m->add(fun(static_cast<ccHObject * (CLMeshDesc::*)()>(&CLMeshDesc::getEntity)), "getEntity");
+				m->add(fun(static_cast<const ccHObject * (CLMeshDesc::*)()const>(&CLMeshDesc::getEntity)), "getEntity");
+
+
+				m->add(chaiscript::user_type<ccCommandLineInterface>(), "ccCommandLineInterface"); //TODO ADD CONSTRUCTOR
+					m->add(chaiscript::user_type<ccCommandLineInterface::Command>(), "Command");
+					
+					m->add(chaiscript::user_type<QSharedPointer<ccCommandLineInterface::Command>>(), "SharedCommand");
+					m->add(fun(&ccCommandLineInterface::Command::process), "process");
+					m->add(fun(&ccCommandLineInterface::Command::m_name), "m_name");
+					m->add(fun(&ccCommandLineInterface::Command::m_keyword), "m_keyword");
+				m->add(fun(&ccCommandLineInterface::IsCommand), "IsCommand");
+				m->add(fun(&ccCommandLineInterface::registerCommand), "registerCommand");
+				m->add(fun(&ccCommandLineInterface::getExportFilename), "getExportFilename"); //TODO ADD DEFAULT PARAMS
+				m->add(fun(&ccCommandLineInterface::exportEntity), "exportEntity"); //TODO ADD DEFAULT PARAMS
+				m->add(fun(&ccCommandLineInterface::saveClouds), "saveClouds"); //TODO ADD DEFAULT PARAMS
+				m->add(fun(&ccCommandLineInterface::saveMeshes), "saveMeshes"); //TODO ADD DEFAULT PARAMS
+				m->add(fun(&ccCommandLineInterface::removeClouds), "removeClouds"); //TODO ADD DEFAULT PARAMS
+				m->add(fun(&ccCommandLineInterface::removeMeshes), "removeMeshes"); //TODO ADD DEFAULT PARAMS
+				m->add(fun(static_cast<QStringList&(ccCommandLineInterface::*)()>(&ccCommandLineInterface::arguments)), "arguments");
+				m->add(fun(static_cast<const QStringList&(ccCommandLineInterface::*)()const>(&ccCommandLineInterface::arguments)), "arguments");
+				m->add(fun(&ccCommandLineInterface::progressDialog), "progressDialog");
+				m->add(fun(&ccCommandLineInterface::widgetParent), "widgetParent");
+					m->add(chaiscript::user_type<ccCommandLineInterface::CLLoadParameters>(), "CLLoadParameters");
+					m->add(chaiscript::constructor<ccCommandLineInterface::CLLoadParameters()>(), "CLLoadParameters");
+					m->add(fun(&ccCommandLineInterface::CLLoadParameters::m_coordinatesShiftEnabled), "m_coordinatesShiftEnabled");
+					m->add(fun(&ccCommandLineInterface::CLLoadParameters::m_coordinatesShift), "m_coordinatesShift");				
+				m->add(fun(&ccCommandLineInterface::fileLoadingParams), "fileLoadingParams");
+				m->add(fun(&ccCommandLineInterface::importFile), "importFile"); //TODO ADD DEFAULT PARAMS
+				m->add(fun(&ccCommandLineInterface::cloudExportFormat), "cloudExportFormat");
+				m->add(fun(&ccCommandLineInterface::cloudExportExt), "cloudExportExt");
+				m->add(fun(&ccCommandLineInterface::meshExportFormat), "meshExportFormat");
+				m->add(fun(&ccCommandLineInterface::meshExportExt), "meshExportExt");
+				m->add(fun(&ccCommandLineInterface::setCloudExportFormat), "setCloudExportFormat");
+				m->add(fun(&ccCommandLineInterface::setMeshExportFormat), "setMeshExportFormat");
+				m->add(fun(&ccCommandLineInterface::print), "print");
+				m->add(fun(&ccCommandLineInterface::warning), "warning");
+				m->add(fun(&ccCommandLineInterface::error), "error");
+				m->add(fun(static_cast<std::vector<CLCloudDesc>&(ccCommandLineInterface::*)()>(&ccCommandLineInterface::clouds)), "clouds");
+				m->add(fun(static_cast<const std::vector<CLCloudDesc>&(ccCommandLineInterface::*)()const>(&ccCommandLineInterface::clouds)), "clouds");
+				m->add(fun(static_cast<std::vector<CLMeshDesc> & (ccCommandLineInterface::*)()>(&ccCommandLineInterface::meshes)), "meshes");
+				m->add(fun(static_cast<const std::vector<CLMeshDesc> & (ccCommandLineInterface::*)()const>(&ccCommandLineInterface::meshes)), "meshes");
+				m->add(fun(&ccCommandLineInterface::toggleSilentMode), "toggleSilentMode");
+				m->add(fun(&ccCommandLineInterface::silentMode), "silentMode");
+				m->add(fun(&ccCommandLineInterface::toggleAutoSaveMode), "toggleAutoSaveMode");
+				m->add(fun(&ccCommandLineInterface::autoSaveMode), "autoSaveMode");
+				m->add(fun(&ccCommandLineInterface::toggleAddTimestamp), "toggleAddTimestamp");
+				m->add(fun(&ccCommandLineInterface::addTimestamp), "addTimestamp");
+				m->add(fun(&ccCommandLineInterface::setNumericalPrecision), "setNumericalPrecision");
+				m->add(fun(&ccCommandLineInterface::numericalPrecision), "numericalPrecision");
+				m->add(fun(&ccCommandLineInterface::coordinatesShiftWasEnabled), "coordinatesShiftWasEnabled");
+				m->add(fun(&ccCommandLineInterface::formerCoordinatesShift), "formerCoordinatesShift");
+				m->add(fun(&ccCommandLineInterface::storeCoordinatesShiftParams), "storeCoordinatesShiftParams");
+				m->add(fun(&ccCommandLineInterface::COMMAND_OPEN_SHIFT_ON_LOAD), "COMMAND_OPEN_SHIFT_ON_LOAD");
+				m->add(fun(&ccCommandLineInterface::COMMAND_OPEN_SHIFT_ON_LOAD_AUTO), "COMMAND_OPEN_SHIFT_ON_LOAD_AUTO");
+				m->add(fun(&ccCommandLineInterface::COMMAND_OPEN_SHIFT_ON_LOAD_FIRST), "COMMAND_OPEN_SHIFT_ON_LOAD_FIRST");
+				m->add(fun(&ccCommandLineInterface::nextCommandIsGlobalShift), "nextCommandIsGlobalShift");
+				m->add(fun(&ccCommandLineInterface::processGlobalShiftCommand), "processGlobalShiftCommand");
+
+
+
+				return m;
+			}
+
+			ModulePtr bs_class_relationships(ModulePtr m = std::make_shared<Module>())
+			{
+				m->add(chaiscript::base_class<CLEntityDesc, CLGroupDesc>());
+				m->add(chaiscript::base_class<CLEntityDesc, CLCloudDesc>());
+				m->add(chaiscript::base_class<CLEntityDesc, CLMeshDesc>());
+
+				return m;
+			}
 
 
 			ModulePtr bootstrap_classes(ModulePtr m = std::make_shared<Module>())
 			{
 				bs_ccMainAppInterface(m);
+				bs_ccCommandLineInterface(m);
+
+				bs_class_relationships(m);
 				return m;
 			}
 		}

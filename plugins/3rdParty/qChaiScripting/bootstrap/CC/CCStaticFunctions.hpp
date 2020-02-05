91137c2041069e1b9b8efa78532fc1faf88c3f23
Chris S Brown <Chris.S.Brown@Boeing.com>
2020-02-04 17:34:18 -0800

    Added distanceComputationTools to chaiscript

--- plugins/3rdParty/qChaiScripting/bootstrap/CC/CCStaticFunctions.hpp
+++ plugins/3rdParty/qChaiScripting/bootstrap/CC/CCStaticFunctions.hpp
@@ -21,7 +21,7 @@
 
 #include <chaiscript/chaiscript.hpp>
 #include <chaiscript/utility/utility.hpp>
-
+#include <DistanceComputationTools.h>
 
 namespace chaiscript
 {
@@ -29,14 +29,40 @@ namespace chaiscript
 	{
 		namespace CC
 		{
-
-
+			using namespace CCLib;
+			ModulePtr bs_DistanceComputationTools(ModulePtr m = std::make_shared<Module>())
+			{
+				m->add(fun(&DistanceComputationTools::computeCloud2CloudDistance), "computeCloud2CloudDistance");
+				m->add(fun(&DistanceComputationTools::computeCloud2MeshDistance), "computeCloud2MeshDistance");
+				m->add(fun(&DistanceComputationTools::computeApproxCloud2CloudDistance), "computeApproxCloud2CloudDistance");
+				m->add(fun(&DistanceComputationTools::computePoint2TriangleDistance), "computePoint2TriangleDistance");
+				m->add(fun(&DistanceComputationTools::computePoint2PlaneDistance), "computePoint2PlaneDistance");
+				m->add(fun(&DistanceComputationTools::computePoint2LineSegmentDistSquared), "computePoint2LineSegmentDistSquared");
+				m->add(fun(&DistanceComputationTools::computeCloud2ConeEquation), "computeCloud2ConeEquation");
+				m->add(fun(&DistanceComputationTools::computeCloud2CylinderEquation), "computeCloud2CylinderEquation");
+				m->add(fun(&DistanceComputationTools::computeCloud2SphereEquation), "computeCloud2SphereEquation");
+				m->add(fun(&DistanceComputationTools::computeCloud2PlaneEquation), "computeCloud2PlaneEquation");
+				m->add(fun(&DistanceComputationTools::computeCloud2RectangleEquation), "computeCloud2RectangleEquation");
+				m->add(fun(&DistanceComputationTools::computeCloud2BoxEquation), "computeCloud2BoxEquation");
+				m->add(fun(&DistanceComputationTools::computeCloud2PolylineEquation), "computeCloud2PolylineEquation");
+				m->add(fun(&DistanceComputationTools::ComputeCloud2PlaneDistance), "ComputeCloud2PlaneDistance");
+				m->add(fun(&DistanceComputationTools::ComputeCloud2PlaneRobustMax), "ComputeCloud2PlaneRobustMax");
+				m->add(fun(&DistanceComputationTools::ComputeCloud2PlaneMaxDistance), "ComputeCloud2PlaneMaxDistance");
+				m->add(fun(&DistanceComputationTools::computeCloud2PlaneDistanceRMS), "computeCloud2PlaneDistanceRMS");
+				m->add(fun(&DistanceComputationTools::ComputeSquareDistToSegment), "ComputeSquareDistToSegment");
+				m->add(fun(&DistanceComputationTools::computeGeodesicDistances), "computeGeodesicDistances");
+				m->add(fun(&DistanceComputationTools::diff), "diff");
+				m->add(fun(&DistanceComputationTools::synchronizeOctrees), "synchronizeOctrees");
+				m->add(fun(&DistanceComputationTools::MultiThreadSupport), "MultiThreadSupport");				
+				return m;
+			}
+			
 
 
 
 			ModulePtr bootstrap_static_functions(ModulePtr m = std::make_shared<Module>())
 			{
-
+				bs_DistanceComputationTools(m);
 				return m;
 			}
 		}

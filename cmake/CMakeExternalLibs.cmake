b5e5dcbb88b82b63c656e6f5ac89b7696bffc5dd
Andy Maloney <asmaloney@gmail.com>
2020-03-30 13:43:03 -0400

    {macOS} Disable OpenMP checking since it causes build issues
    Handling OpenMP on macOS will wait until a future release.

--- cmake/CMakeExternalLibs.cmake
+++ cmake/CMakeExternalLibs.cmake
@@ -67,11 +67,13 @@ endif()
 # ------------------------------------------------------------------------------
 # OpenMP
 # ------------------------------------------------------------------------------
-find_package(OpenMP QUIET)
-if (OPENMP_FOUND)
-	message(STATUS "OpenMP found")
-    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
-    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+if ( NOT APPLE )
+	find_package(OpenMP QUIET)
+	if (OPENMP_FOUND)
+		message(STATUS "OpenMP found")
+		set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+		set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+	endif()
 endif()
 
 # Intel's Threading Building Blocks (TBB)

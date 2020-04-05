fc0aa52429eb34b7d4000c3f6ebe50ade603729f
Paul RASCLE <paul.rascle@openfields.fr>
2020-04-05 11:02:26 +0200

    [API Python] license headers

--- CC/include/ccTrace.h
+++ CC/include/ccTrace.h
@@ -1,9 +1,20 @@
-/*
- * ccTrace.h
- *
- *  Created on: 14 mars 2020
- *      Author: paul
- */
+//##########################################################################
+//#                                                                        #
+//#                               CC                                       #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU Library General Public License as       #
+//#  published by the Free Software Foundation; version 2 or later of the  #
+//#  License.                                                              #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#          Copyright 2020 Paul RASCLE www.openfields.fr                  #
+//#                                                                        #
+//##########################################################################
 
 #ifndef CC_INCLUDE_CCTRACE_H_
 #define CC_INCLUDE_CCTRACE_H_
--- CCPAPI/test.py
+++ CCPAPI/test.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
+##########################################################################
+#                                                                        #
+#                               CCPAPI                                   #
+#                                                                        #
+#  This program is free software; you can redistribute it and/or modify  #
+#  it under the terms of the GNU Library General Public License as       #
+#  published by the Free Software Foundation; version 2 or later of the  #
+#  License.                                                              #
+#                                                                        #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+#  GNU General Public License for more details.                          #
+#                                                                        #
+#          Copyright 2020 Paul RASCLE www.openfields.fr                  #
+#                                                                        #
+##########################################################################
+
 import sys
 sys.path.append('/home/paul/projets/CloudCompare/install/lib/cloudcompare')
 from PyQt5.QtWidgets import QApplication

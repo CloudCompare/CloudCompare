89c9c3b58e8cf2bb1041ada7fa9aa5f48a5d6d43
Andy Maloney <asmaloney@gmail.com>
2020-04-08 17:39:36 -0400

    Use Ui class as member in ccSORFilterDlg (#1124)
    This one required a few more changes because the main window was using the UI fields directly. So I added accessors for them.

--- qCC/ccSORFilterDlg.h
+++ qCC/ccSORFilterDlg.h
@@ -18,17 +18,32 @@
 #ifndef CC_SOR_FILTER_DLG_HEADER
 #define CC_SOR_FILTER_DLG_HEADER
 
-#include <ui_sorFilterDlg.h>
+#include <QDialog>
+
+namespace Ui {
+	class SorFilterDialog;
+}
 
 //! Dialog to choose which dimension(s) (X, Y or Z) should be exported as SF(s)
-class ccSORFilterDlg : public QDialog, public Ui::SorFilterDialog
+class ccSORFilterDlg : public QDialog
 {
 	Q_OBJECT
 
 public:
 
 	//! Default constructor
-	explicit ccSORFilterDlg(QWidget* parent = 0);
+	explicit ccSORFilterDlg(QWidget* parent = nullptr);
+	
+	~ccSORFilterDlg();
+	
+	int KNN() const;
+	void setKNN( int knn );
+	
+	double nSigma() const;
+	void setNSigma( double nSigma );
+	
+private:
+	Ui::SorFilterDialog* m_ui;
 };
 
 #endif //CC_SOR_FILTER_DLG_HEADER

e7d4279ee188997a56748ff8d73281a6b8183e09
Andy Maloney <asmaloney@gmail.com>
2020-04-09 15:56:02 -0400

    Fix comments [skip ci]

--- plugins/core/Standard/qHoughNormals/src/qHoughNormalsDialog.h
+++ plugins/core/Standard/qHoughNormals/src/qHoughNormalsDialog.h
@@ -1,6 +1,6 @@
 //##########################################################################
 //#                                                                        #
-//#                       CLOUDCOMPARE PLUGIN: qHPR                        #
+//#                       CLOUDCOMPARE PLUGIN: qHoughNormals               #
 //#                                                                        #
 //#  This program is free software; you can redistribute it and/or modify  #
 //#  it under the terms of the GNU General Public License as published by  #

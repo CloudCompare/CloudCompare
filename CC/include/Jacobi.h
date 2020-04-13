55faa3bd15b93f5d958e11120b789955ae22bcfe
Andy Maloney <asmaloney@gmail.com>
2020-04-13 13:34:45 -0400

    Fix license on 3 CCLib files (#1138)
    These were mistakenly added to CCLib as GPL rather than LGPL.
    
    As they are copyright by the CC project, and only Andy and Daniel have modified them, we are choosing to relicense them as LGPL.
    
    [skip ci]

--- CC/include/Jacobi.h
+++ CC/include/Jacobi.h
@@ -1,10 +1,11 @@
 //##########################################################################
 //#                                                                        #
-//#                              CLOUDCOMPARE                              #
+//#                               CCLIB                                    #
 //#                                                                        #
 //#  This program is free software; you can redistribute it and/or modify  #
-//#  it under the terms of the GNU General Public License as published by  #
-//#  the Free Software Foundation; version 2 or later of the License.      #
+//#  it under the terms of the GNU Library General Public License as       #
+//#  published by the Free Software Foundation; version 2 or later of the  #
+//#  License.                                                              #
 //#                                                                        #
 //#  This program is distributed in the hope that it will be useful,       #
 //#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #

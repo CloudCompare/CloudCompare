cfec7baec99b1e71871e945ba1da681aabeeb739
Paul RASCLE <paul.rascle@openfields.fr>
2020-04-15 10:42:44 +0200

    [API Python] move FindPYQTSIP from CMake directory to CCPAPI directory

--- CCPAPI/FindPYQTSIP.cmake
+++ CCPAPI/FindPYQTSIP.cmake
@@ -1,6 +1,6 @@
 ##########################################################################
 #                                                                        #
-#                               CMAKE / CLOUDCOMPARE                     #
+#                               CCPAPI                                   #
 #                                                                        #
 #  This program is free software; you can redistribute it and/or modify  #
 #  it under the terms of the GNU Library General Public License as       #

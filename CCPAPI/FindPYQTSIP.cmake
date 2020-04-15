456acf7acd80017f95b43da0e121d266fb1b71f2
Paul RASCLE <paul.rascle@openfields.fr>
2020-04-15 11:07:27 +0200

    [API Python] use lowercase CMake function names (https://cliutils.gitlab.io/modern-cmake/chapters/intro/dodonot.html)

--- CCPAPI/FindPYQTSIP.cmake
+++ CCPAPI/FindPYQTSIP.cmake
@@ -28,48 +28,50 @@
 #   PYQT_SIPFLAGS
 #
 
-IF(NOT PYQTSIP_FIND_QUIETLY)
-  MESSAGE(STATUS "Looking for PYQT5 and SIP ...")
-ENDIF()
+if(NOT PYQTSIP_FIND_QUIETLY)
+  message(STATUS "Looking for PYQT5 and SIP ...")
+endif()
 
-FIND_PROGRAM(PYQT_PYUIC_EXECUTABLE NAMES pyuic5 pyuic5.bat DOC "full path of pyuic5 script")
-FIND_PROGRAM(PYQT_PYRCC_EXECUTABLE NAMES pyrcc5 pyrcc5.bat DOC "full path of pyrcc5 script")
+find_program(PYQT_PYUIC_EXECUTABLE NAMES pyuic5 pyuic5.bat DOC "full path of pyuic5 script")
+find_program(PYQT_PYRCC_EXECUTABLE NAMES pyrcc5 pyrcc5.bat DOC "full path of pyrcc5 script")
 
-EXECUTE_PROCESS(COMMAND ${PYTHON_EXECUTABLE} -c "import sys;
+execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import sys;
 hints=' '.join(sys.path);
 sys.stdout.write(hints)"
 OUTPUT_VARIABLE _tmpPythonPath)
-SEPARATE_ARGUMENTS(_tmpPythonPath)
-FIND_PATH(PYQT_PYTHONPATH PyQt5/__init__.py HINTS ${_tmpPythonPath} DOC "directory path of PyQt5 Python module")
-MESSAGE(STATUS "PYQT_PYTHONPATH: " ${PYQT_PYTHONPATH})
 
-FIND_PROGRAM(SIP_EXECUTABLE sip DOC "full path of executable: sip")
-MESSAGE("SIP_EXECUTABLE: " ${SIP_EXECUTABLE})
+separate_arguments(_tmpPythonPath)
+find_path(PYQT_PYTHONPATH PyQt5/__init__.py HINTS ${_tmpPythonPath} DOC "directory path of PyQt5 Python module")
+message(STATUS "PYQT_PYTHONPATH: " ${PYQT_PYTHONPATH})
 
-FIND_PATH(SIP_INCLUDE_DIR sip.h PATH_SUFFIXES python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR} DOC "directory path of include: sip.h")
-MESSAGE(STATUS "SIP_INCLUDE_DIR: " ${SIP_INCLUDE_DIR})
+find_program(SIP_EXECUTABLE sip DOC "full path of executable: sip")
+message("SIP_EXECUTABLE: " ${SIP_EXECUTABLE})
 
-FIND_PATH(PYQT_SIPS QtGui/QtGuimod.sip HINTS "/usr/share/sip/PyQt5" DOC "directory path of QtGui/QtGuimod.sip")
-MESSAGE(STATUS "PYQT_SIPS: " ${PYQT_SIPS})
+find_path(SIP_INCLUDE_DIR sip.h PATH_SUFFIXES python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR} DOC "directory path of include: sip.h")
+message(STATUS "SIP_INCLUDE_DIR: " ${SIP_INCLUDE_DIR})
 
-IF(SIP_INCLUDE_DIR)
-  GET_FILENAME_COMPONENT(SIP_PYTHONPATH "${SIP_INCLUDE_DIR}" PATH)
-  GET_FILENAME_COMPONENT(SIP_PYTHONPATH "${SIP_PYTHONPATH}" PATH)
-  SET(SIP_PYTHONPATH "${SIP_PYTHONPATH}/lib/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/site-packages")
-  MESSAGE(STATUS "SIP_PYTHONPATH: " ${SIP_PYTHONPATH})
-ENDIF()
+find_path(PYQT_SIPS QtGui/QtGuimod.sip HINTS "/usr/share/sip/PyQt5" DOC "directory path of QtGui/QtGuimod.sip")
+message(STATUS "PYQT_SIPS: " ${PYQT_SIPS})
 
-EXECUTE_PROCESS(COMMAND ${PYTHON_EXECUTABLE} -c "import sys; 
+if(SIP_INCLUDE_DIR)
+  get_filename_component(SIP_PYTHONPATH "${SIP_INCLUDE_DIR}" PATH)
+  get_filename_component(SIP_PYTHONPATH "${SIP_PYTHONPATH}" PATH)
+  set(SIP_PYTHONPATH "${SIP_PYTHONPATH}/lib/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/site-packages")
+  message(STATUS "SIP_PYTHONPATH: " ${SIP_PYTHONPATH})
+endif()
+
+execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import sys; 
 sys.path[:0] = '${PYQT_PYTHONPATH}'.split(';');
 from PyQt5.QtCore import PYQT_CONFIGURATION;
 sys.stdout.write(PYQT_CONFIGURATION['sip_flags'])"
 OUTPUT_VARIABLE PYQT_SIPFLAGS)
-SEPARATE_ARGUMENTS(PYQT_SIPFLAGS)
 
-SET(PYQT_SIPFLAGS ${PYQT_SIPFLAGS} -I "${PYQT_SIPS}")
-MESSAGE(STATUS "PYQT_SIPFLAGS: " "${PYQT_SIPFLAGS}")
+separate_arguments(PYQT_SIPFLAGS)
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(PYQTSIP REQUIRED_VARS PYQT_PYUIC_EXECUTABLE SIP_INCLUDE_DIR SIP_EXECUTABLE PYQT_SIPFLAGS)
+set(PYQT_SIPFLAGS ${PYQT_SIPFLAGS} -I "${PYQT_SIPS}")
+message(STATUS "PYQT_SIPFLAGS: " "${PYQT_SIPFLAGS}")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(PYQTSIP REQUIRED_VARS PYQT_PYUIC_EXECUTABLE SIP_INCLUDE_DIR SIP_EXECUTABLE PYQT_SIPFLAGS)
 
 

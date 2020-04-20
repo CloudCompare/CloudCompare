6e3606fd2ea3b42138b731897b212b158e82e94b
Andy Maloney <asmaloney@gmail.com>
2020-04-20 11:39:11 -0400

    {cmake} Clean up dxflib and shapelib (#1146)
    - move them to extern
    - rename dxflib directory to remove version number
    - move their .cmake files to cmake directory
    - default to them being ON
    - clean up places they are linked in the cmake files (no need to add compile defs or includes - that's part of the target)

--- cmake/DxfLibSupport.cmake
+++ cmake/DxfLibSupport.cmake
@@ -2,18 +2,15 @@
 # Dxf Lib + CMake support for CloudCompare
 # ------------------------------------------------------------------------------
 
-OPTION( OPTION_USE_DXF_LIB "Build with Ribbonsoft's DXF Lib (AutoCAD DXF files support)" OFF )
+OPTION( OPTION_USE_DXF_LIB "Build with Ribbonsoft's DXF Lib (AutoCAD DXF files support)" ON )
 if( ${OPTION_USE_DXF_LIB} )
-	# set( DXF_LIB_DIR )
-	set( DXF_LIB_DIR "contrib/dxflib-3.17.0"  CACHE PATH "DXF lib path" )
+	set( DXF_LIB_DIR "extern/dxflib" CACHE PATH "DXF lib path" )
 	add_subdirectory( "${DXF_LIB_DIR}" )
-	include_directories( "${DXF_LIB_DIR}/src" )
 endif()
 
 # Link project with dxflib library
 function( target_link_DXFLIB )
 	if( ${OPTION_USE_DXF_LIB} )
-		target_link_libraries( ${PROJECT_NAME} DXF_LIB )	
-		target_compile_definitions( ${PROJECT_NAME} PUBLIC CC_DXF_SUPPORT )
+		target_link_libraries( ${PROJECT_NAME} dxflib )	
 	endif()
 endfunction()
--- cmake/ShapeLibSupport.cmake
+++ cmake/ShapeLibSupport.cmake
@@ -2,16 +2,15 @@
 # ShapeLib + CMake support for CloudCompare
 # ------------------------------------------------------------------------------
 
-OPTION( OPTION_USE_SHAPE_LIB "Build with ShapeLib (SHP files support)" OFF )
+OPTION( OPTION_USE_SHAPE_LIB "Build with ShapeLib (SHP files support)" ON )
 if( ${OPTION_USE_SHAPE_LIB} )
-	add_subdirectory(contrib/shapelib)
-	include_directories( ${SHAPELIB_SOURCE_DIR} )
+    set( SHAPELIB_LIB_DIR "extern/shapelib" CACHE PATH "shapelib lib path" )
+	add_subdirectory( "${SHAPELIB_LIB_DIR}" )
 endif()
 
 # Link project with shapelib library
 function( target_link_SHAPE_LIB ) # 2 arguments: ARGV0 = project name
 	if( ${OPTION_USE_SHAPE_LIB} )
-		target_link_libraries( ${PROJECT_NAME} SHAPELIB )	
-		target_compile_definitions( ${PROJECT_NAME} PUBLIC CC_SHP_SUPPORT )
+		target_link_libraries( ${PROJECT_NAME} shapelib )	
 	endif()
 endfunction()

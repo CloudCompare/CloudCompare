1556db8ca64d660c2606b7c4b48567b8d6d0a0d9
Andy Maloney <asmaloney@gmail.com>
2020-04-21 16:32:11 -0400

    {cmake} Remove policy CMP0020 (linking Qt::WinMain)
    Linking to Qt::WinMain explcitly isn't required when using CMake >= 3.0
    
    https://cmake.org/cmake/help/v3.10/policy/CMP0020.html

--- cmake/CMakePolicies.cmake
+++ cmake/CMakePolicies.cmake
@@ -1,7 +1,5 @@
 # Eliminate warnings with newer versions of CMake
 if ( WIN32 )
-	# https://cmake.org/cmake/help/v3.0/policy/CMP0020.html
-    cmake_policy(SET CMP0020 NEW)
 	# https://cmake.org/cmake/help/v3.0/policy/CMP0071.html
 	cmake_policy(SET CMP0071 NEW)
 endif()

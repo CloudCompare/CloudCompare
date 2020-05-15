504c211ff6bcfc3e06885b802855ef70317cb60d
Andy Maloney <asmaloney@gmail.com>
2020-05-15 07:45:00 -0400

    {cmake} Replace CMakePluginTemplate with a function to install plugins (#1221)
    Resolves #1174

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -5,15 +5,15 @@
 # access them.
 #
 # Arguments:
-#   TARGET The name of the library target
+#	TARGET The name of the library target
 function( InstallSharedLibrary )
-    cmake_parse_arguments(
-            INSTALL_SHARED_LIB
-            ""
-            "TARGET"
-            ""
-            ${ARGN}
-        )
+	cmake_parse_arguments(
+			INSTALL_SHARED_LIB
+			""
+			"TARGET"
+			""
+			${ARGN}
+	)
 
 	message( STATUS "Install shared library: ${INSTALL_SHARED_LIB_TARGET}")
 
@@ -23,5 +23,144 @@ function( InstallSharedLibrary )
 		endforeach()
 	else()
 		install_shared( ${INSTALL_SHARED_LIB_TARGET} ${CMAKE_INSTALL_LIBDIR}/cloudcompare )
+	endif()
+endfunction()
+
+# InstallPlugins should be called once for each application.
+# This function installs the plugin types that are requested in TYPES to the path specified by DEST_FOLDER.
+# If it is a gl plugin with shaders, install the shaders to SHADER_DEST_FOLDER.
+#
+# Arguments:
+#	DEST_FOLDER Where to install the plugins.
+#	SHADER_DEST_FOLDER Where to install the shaders for the plugins.
+#	TYPES Semicolon-separated list of plugin types to install (valid: gl, io, standard). If not specified, install all.
+function( InstallPlugins )
+	cmake_parse_arguments(
+			INSTALL_PLUGINS
+			""
+			"DEST_FOLDER;SHADER_DEST_FOLDER"
+			"TYPES"
+			${ARGN}
+	)
+	
+	# Check the types we need to install
+	set( VALID_TYPES "gl" "io" "standard" )
+	
+	# If TYPES was not specified, use all of them
+	if( NOT INSTALL_PLUGINS_TYPES )
+		set( INSTALL_PLUGINS_TYPES "${VALID_TYPES}" )
+	else()
+		foreach( type ${INSTALL_PLUGINS_TYPES} )
+			if( NOT "${type}" IN_LIST VALID_TYPES )		   
+				# In cmake 3.12:
+				# list( JOIN VALID_TYPES ", " VALID_TYPES_STR )
+				string( REPLACE ";" ", " VALID_TYPES_STR "${VALID_TYPES}" )
+				
+				message( FATAL_ERROR "InstallPlugins: Did not find proper TYPES. Valid values are: ${VALID_TYPES_STR}" )
+			endif()
+		endforeach()
+	endif()
+	
+	message( STATUS "Install plugins" )
+	message( STATUS " Types: ${INSTALL_PLUGINS_TYPES}" )
+	
+	# Check our destination folder is valid
+	if( NOT INSTALL_PLUGINS_DEST_FOLDER )
+		message( FATAL_ERROR "InstallPlugins: DEST_FOLDER not specified" )
+	endif()
+	
+	# If we have gl plugins, check that our shader destination folder is valid
+	if( "gl" IN_LIST VALID_TYPES )
+		if( NOT INSTALL_PLUGINS_SHADER_DEST_FOLDER )
+			message( FATAL_ERROR "InstallPlugins: SHADER_DEST_FOLDER not specified" )
+		endif()
+	endif()
+	
+	# Install the requested plugins in the DEST_FOLDER
+	foreach( plugin_target ${CC_PLUGIN_TARGET_LIST} )
+		get_target_property( plugin_type ${plugin_target} PLUGIN_TYPE )
+		
+		if( "${plugin_type}" IN_LIST INSTALL_PLUGINS_TYPES )
+			message( STATUS " Install ${plugin_target} (${plugin_type})" )
+			
+			_InstallPluginTarget(
+				TARGET ${plugin_target}
+				DEST_FOLDER ${INSTALL_PLUGINS_DEST_FOLDER}
+			)		
+			
+			if( "${plugin_type}" STREQUAL "gl" )
+				get_target_property( SHADER_FOLDER_NAME ${plugin_target} SHADER_FOLDER_NAME )
+				get_target_property( SHADER_FOLDER_PATH ${plugin_target} SHADER_FOLDER_PATH )
+				
+				if( EXISTS "${SHADER_FOLDER_PATH}" )
+					message( STATUS "  + shader: ${SHADER_FOLDER_NAME} (${SHADER_FOLDER_PATH})" )
+					
+					get_target_property( shader_files ${plugin_target} SOURCES )
+					list( FILTER shader_files INCLUDE REGEX ".*\.vert|frag" )					
+					
+					foreach( filename ${shader_files} )
+						install(
+							FILES ${filename}
+							DESTINATION "${INSTALL_PLUGINS_SHADER_DEST_FOLDER}/${SHADER_FOLDER_NAME}"
+						)
+					endforeach()
+				endif()
+			endif()
+		endif()
+	endforeach()	
+endfunction()
+
+# _InstallPluginTarget should only be called by InstallPlugins above.
+# It was factored out to provide cmake < 3.13 a way to install plugins.
+#
+# Arguments:
+#	DEST_FOLDER Where to install the plugins.
+#	TARGET The name of the plugin target
+function( _InstallPluginTarget )
+	cmake_parse_arguments(
+			INSTALL_PLUGIN_TARGET
+			""
+			"DEST_FOLDER;TARGET"
+			""
+			${ARGN}
+	)
+	
+	# For readability
+	set( plugin_target "${INSTALL_PLUGIN_TARGET_TARGET}" )
+	
+	# Before CMake 3.13, install(TARGETS) would only accept targets created in the same directory scope
+	# This makes it difficult to work with submodules.
+	# This can be cleaned up when we move to a minimum CMake of 3.13 or higher
+	# https://gitlab.kitware.com/cmake/cmake/-/merge_requests/2152
+	if ( ${CMAKE_VERSION} VERSION_LESS "3.13.0" )
+		# Basic hack: construct the name of the plugin dynamic library ("target_plugin") and install using
+		# install(FILES) instead of install(TARGETS)
+		
+		if ( APPLE OR UNIX )
+			set( lib_prefix "lib" )
+		endif()
+		
+		if ( CMAKE_BUILD_TYPE STREQUAL "Debug" )
+			get_target_property( lib_postfix ${plugin_target} DEBUG_POSTFIX)
+		endif()
+		
+		get_target_property( target_bin_dir ${plugin_target} BINARY_DIR )
+		
+		set( target_plugin "${target_bin_dir}/${lib_prefix}${plugin_target}${lib_postfix}${CMAKE_SHARED_LIBRARY_SUFFIX}" )
+				
+		if ( WIN32 )
+			copy_files( "${target_plugin}" "${INSTALL_PLUGIN_TARGET_DEST_FOLDER}" 1 )
+		else()
+			copy_files( "${target_plugin}" "${INSTALL_PLUGIN_TARGET_DEST_FOLDER}" )
+		endif()
+	else()	
+		if( WIN32 )
+			install_shared( ${plugin_target} ${INSTALL_PLUGIN_TARGET_DEST_FOLDER} 1 )
+		else()
+			install( TARGETS ${plugin_target}
+				LIBRARY DESTINATION ${INSTALL_PLUGIN_TARGET_DEST_FOLDER}
+				COMPONENT Runtime
+			)
+		endif()
 	endif()
 endfunction()
--- plugins/cmake/Plugins.cmake
+++ plugins/cmake/Plugins.cmake
@@ -1,128 +1,149 @@
+set( CC_PLUGIN_TARGET_LIST "" CACHE INTERNAL "Internal plugin list" )
+
 # AddPlugin should be called once for each plugin.
 # This function sets up a target for the plugin, sets up default properties, and sets the target
 # to link to the necessary libraries.
 #
 # Arguments:
-#   NAME The name of the plugin (this is the target)
-#   TYPE One of "gl", "io", or "standard". If TYPE is not specified, it will default to "standard".
-#   SHADER_FOLDER If it is a gl plugin, this is the folder containing the shaders. Ignored otherwise.
+#	NAME The name of the plugin (this is the target)
+#	TYPE One of "gl", "io", or "standard". If TYPE is not specified, it will default to "standard".
+#	SHADER_FOLDER If it is a gl plugin, this is the folder containing the shaders. Ignored otherwise.
 function( AddPlugin )
-    cmake_parse_arguments(
-            ADD_PLUGIN
-            ""
-            "NAME;TYPE;SHADER_FOLDER"
-            ""
-            ${ARGN}
-        )
-    
-    # For readability
-    set( PLUGIN_TARGET "${ADD_PLUGIN_NAME}" )
-    
-    # First check our TYPE
-    
-    # If none given, make it "standard"
-    if ( NOT ADD_PLUGIN_TYPE )
-        set( ADD_PLUGIN_TYPE "standard" )
-    endif()
-    
-    # Check that we were given a valid type
-    set( VALID_TYPES "gl" "io" "standard" )
-    if( NOT "${ADD_PLUGIN_TYPE}" IN_LIST VALID_TYPES )        
-        # In cmake 3.12:
-        # list( JOIN VALID_TYPES ", " VALID_TYPES_STR )
-        string( REPLACE ";" ", " VALID_TYPES_STR "${VALID_TYPES}" )
-        
-        message( FATAL_ERROR "AddPlugin: Did not find proper TYPE. Valid values are: ${VALID_TYPES_STR}" )
-    endif()
-    
-    # Create our target
-    add_library( ${PLUGIN_TARGET} SHARED )
-    
-    # Set default properties
-    set_target_properties( ${PLUGIN_TARGET}
-        PROPERTIES
-            AUTOUIC ON # FIXME Remove after everything has moved to targets and we can set it globally
-            AUTOUIC_SEARCH_PATHS ${CMAKE_CURRENT_SOURCE_DIR}/ui
-            PLUGIN_TYPE "${ADD_PLUGIN_TYPE}"
-    )
-    
-    # Keep track of our shader folder if it was given
-    if( ("${ADD_PLUGIN_TYPE}" STREQUAL "gl") AND ADD_PLUGIN_SHADER_FOLDER )
-        # Because they are added relative to the CMakeLists file, add the path
-        set( SHADER_FOLDER "${CMAKE_CURRENT_SOURCE_DIR}/shaders/${ADD_PLUGIN_SHADER_FOLDER}" )
-        
-        # Check that the folder exists
-        if( NOT EXISTS "${SHADER_FOLDER}" )
-            message( FATAL_ERROR "AddPlugin: SHADER_FOLDER does not exist: ${SHADER_FOLDER}" )
-        endif()
-        
-        # Add it as a property
-        set_target_properties( ${PLUGIN_TARGET}
-            PROPERTIES
-                SHADER_FOLDER_NAME "${ADD_PLUGIN_SHADER_FOLDER}"
-                SHADER_FOLDER_PATH "${SHADER_FOLDER}"
-        )
-    endif()
-    
-    if( WIN32 )
-        # Plugins need the QT_NO_DEBUG preprocessor in release!
-        target_compile_definitions( ${PLUGIN_TARGET} PRIVATE $<$<CONFIG:Release>:QT_NO_DEBUG> )
-    endif()
-    
-    # Add .qrc file to sources
-    get_filename_component( FOLDER_NAME ${CMAKE_CURRENT_SOURCE_DIR} NAME )
-    set( QRC_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${FOLDER_NAME}.qrc" )
-    if( EXISTS "${QRC_FILE}" )
-        target_sources( ${PLUGIN_TARGET} PRIVATE ${QRC_FILE} )
-    else()
-        message( FATAL_ERROR "Missing qrc file: ${QRC_FILE}" )
-    endif()
+	cmake_parse_arguments(
+			ADD_PLUGIN
+			""
+			"NAME;TYPE;SHADER_FOLDER"
+			""
+			${ARGN}
+	)
+	
+	# For readability
+	set( PLUGIN_TARGET "${ADD_PLUGIN_NAME}" )
+	
+	set( CC_PLUGIN_TARGET_LIST "${CC_PLUGIN_TARGET_LIST};${PLUGIN_TARGET}" CACHE INTERNAL "Internal plugin list" )
+	
+	# First check our TYPE
+	
+	# If none given, make it "standard"
+	if ( NOT ADD_PLUGIN_TYPE )
+		set( ADD_PLUGIN_TYPE "standard" )
+	endif()
+	
+	# Check that we were given a valid type
+	set( VALID_TYPES "gl" "io" "standard" )
+	if( NOT "${ADD_PLUGIN_TYPE}" IN_LIST VALID_TYPES )		  
+		# In cmake 3.12:
+		# list( JOIN VALID_TYPES ", " VALID_TYPES_STR )
+		string( REPLACE ";" ", " VALID_TYPES_STR "${VALID_TYPES}" )
+		
+		message( FATAL_ERROR "AddPlugin: Did not find proper TYPE. Valid values are: ${VALID_TYPES_STR}" )
+	endif()
+	
+	# Create our target
+	add_library( ${PLUGIN_TARGET} SHARED )
+	
+	# Set default properties
+	set_target_properties( ${PLUGIN_TARGET}
+		PROPERTIES
+			AUTOUIC ON # FIXME Remove after everything has moved to targets and we can set it globally
+			AUTOUIC_SEARCH_PATHS ${CMAKE_CURRENT_SOURCE_DIR}/ui
+			PLUGIN_TYPE "${ADD_PLUGIN_TYPE}"
+	)
+	
+	# Keep track of our shader folder if it was given
+	if( ("${ADD_PLUGIN_TYPE}" STREQUAL "gl") AND ADD_PLUGIN_SHADER_FOLDER )
+		# Because they are added relative to the CMakeLists file, add the path
+		set( SHADER_FOLDER "${CMAKE_CURRENT_SOURCE_DIR}/shaders/${ADD_PLUGIN_SHADER_FOLDER}" )
+		
+		# Check that the folder exists
+		if( NOT EXISTS "${SHADER_FOLDER}" )
+			message( FATAL_ERROR "AddPlugin: SHADER_FOLDER does not exist: ${SHADER_FOLDER}" )
+		endif()
+		
+		# Add it as a property
+		set_target_properties( ${PLUGIN_TARGET}
+			PROPERTIES
+				SHADER_FOLDER_NAME "${ADD_PLUGIN_SHADER_FOLDER}"
+				SHADER_FOLDER_PATH "${SHADER_FOLDER}"
+		)
+	endif()
+	
+	if( WIN32 )
+		# Plugins need the QT_NO_DEBUG preprocessor in release!
+		target_compile_definitions( ${PLUGIN_TARGET} PRIVATE $<$<CONFIG:Release>:QT_NO_DEBUG> )
+	endif()
+	
+	# Add .qrc file to sources
+	get_filename_component( FOLDER_NAME ${CMAKE_CURRENT_SOURCE_DIR} NAME )
+	set( QRC_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${FOLDER_NAME}.qrc" )
+	if( EXISTS "${QRC_FILE}" )
+		target_sources( ${PLUGIN_TARGET} PRIVATE ${QRC_FILE} )
+	else()
+		message( FATAL_ERROR "Missing qrc file: ${QRC_FILE}" )
+	endif()
 
-    # Add info.json to sources
-    set( INFO_FILE "${CMAKE_CURRENT_SOURCE_DIR}/info.json" )
-    if( EXISTS "${INFO_FILE}" )
-        target_sources( ${PLUGIN_TARGET} PRIVATE ${INFO_FILE} )
-    else()
-        message( FATAL_ERROR "Missing info.json file: ${INFO_FILE}" )
-    endif()
+	# Add info.json to sources
+	set( INFO_FILE "${CMAKE_CURRENT_SOURCE_DIR}/info.json" )
+	if( EXISTS "${INFO_FILE}" )
+		target_sources( ${PLUGIN_TARGET} PRIVATE ${INFO_FILE} )
+	else()
+		message( FATAL_ERROR "Missing info.json file: ${INFO_FILE}" )
+	endif()
    
-    # Link to required libraries
-    target_link_libraries( ${PLUGIN_TARGET}
-        CCPluginAPI
-        CCPluginStub
-        QCC_GL_LIB
-        Qt5::Concurrent
-        Qt5::OpenGL
-        Qt5::Widgets
-    )
+	# Link to required libraries
+	target_link_libraries( ${PLUGIN_TARGET}
+		CCPluginAPI
+		CCPluginStub
+		QCC_GL_LIB
+		Qt5::Concurrent
+		Qt5::OpenGL
+		Qt5::Widgets
+	)
 
-    message( STATUS "Added ${ADD_PLUGIN_TYPE} plugin: ${ADD_PLUGIN_NAME}")
+	# On macOS, copy the plugin to the ccPlugins directory at the top level
+	# post build so we can find it without installing everything.
+	if( APPLE )
+		set( PLUGINS_OUTPUT_DIR "${CMAKE_BINARY_DIR}/ccPlugins" )
+		
+		if( NOT EXISTS PLUGINS_OUTPUT_DIR )
+			file( MAKE_DIRECTORY "${PLUGINS_OUTPUT_DIR}" )
+		endif()
+		
+		add_custom_command( TARGET ${PLUGIN_TARGET} POST_BUILD
+			COMMAND
+				${CMAKE_COMMAND} -E copy_if_different
+					$<TARGET_FILE:${PLUGIN_TARGET}>
+					${PLUGINS_OUTPUT_DIR}
+		)
+	endif()
+
+	message( STATUS "Added ${ADD_PLUGIN_TYPE} plugin: ${ADD_PLUGIN_NAME}")
 endfunction()
 
 # Documentation of custom properties
 define_property( TARGET
-    PROPERTY
-        PLUGIN_TYPE
-    BRIEF_DOCS
-        "Type of plugin: gl, io, or standard."
-    FULL_DOCS
-        "Type of plugin: gl, io, or standard. Used to decide where to install this plugin."
+	PROPERTY
+		PLUGIN_TYPE
+	BRIEF_DOCS
+		"Type of plugin: gl, io, or standard."
+	FULL_DOCS
+		"Type of plugin: gl, io, or standard. Used to decide where to install this plugin."
 )
 
 define_property( TARGET
-    PROPERTY
-        SHADER_FOLDER_NAME
-    BRIEF_DOCS
-        "Name of the shader folder for a gl plugin."
-    FULL_DOCS
-        "Name of the shader folder for a gl plugin. Used as the name of the folder when installing the shader files."
+	PROPERTY
+		SHADER_FOLDER_NAME
+	BRIEF_DOCS
+		"Name of the shader folder for a gl plugin."
+	FULL_DOCS
+		"Name of the shader folder for a gl plugin. Used as the name of the folder when installing the shader files."
 )
 
 define_property( TARGET
-    PROPERTY
-        SHADER_FOLDER_PATH
-    BRIEF_DOCS
-        "Path to the folder containing the shader files for a gl plugin."
-    FULL_DOCS
-        "Path to the folder containing the shader files for a gl plugin. Files are copied from here when installing."
+	PROPERTY
+		SHADER_FOLDER_PATH
+	BRIEF_DOCS
+		"Path to the folder containing the shader files for a gl plugin."
+	FULL_DOCS
+		"Path to the folder containing the shader files for a gl plugin. Files are copied from here when installing."
 )

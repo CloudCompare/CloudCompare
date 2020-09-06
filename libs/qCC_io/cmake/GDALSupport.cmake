1d5e882b8d46d6bfafd1273608eb257a7fc62f46
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2020-09-06 20:28:29 +0200

    Cleaner GDAL DLL export strategy

--- libs/qCC_io/cmake/GDALSupport.cmake
+++ libs/qCC_io/cmake/GDALSupport.cmake
@@ -27,17 +27,67 @@ function( target_link_GDAL ) # ARGV0 = project name
 	
 	if( WIN32 )
 		#install DLLs
-		message( STATUS "Looking for GDAL DLLs" )
+		message( STATUS "Looking for GDAL DLLs in: " ${GDAL_BIN_DIR} )
+		file( GLOB GDAL_DLL_FILES ${GDAL_BIN_DIR}/gdal*.dll )
 
-		file( GLOB GDAL_DLL_FILES ${GDAL_BIN_DIR}/*.dll )
-		message( STATUS "Looked in: " ${GDAL_BIN_DIR} )
-		message( STATUS ${GDAL_DLL_FILES} )
-
-		file( GLOB GDAL_DLL_FILES2 ${GDAL_INCLUDE_DIR}/../bin/*.dll )
-		message( STATUS "Looked in: " ${GDAL_INCLUDE_DIR}/../bin )
-		message( STATUS ${GDAL_DLL_FILES2} )
-
+		if (GDAL_VERSION_3)
+			set (  GDAL_DEP_DLL_FILES	${GDAL_BIN_DIR}/ogdi.dll
+										${GDAL_BIN_DIR}/expat.dll
+										${GDAL_BIN_DIR}/libpq.dll
+										${GDAL_BIN_DIR}/szip.dll
+										${GDAL_BIN_DIR}/netcdf.dll
+										${GDAL_BIN_DIR}/sqlite3.dll
+										${GDAL_BIN_DIR}/spatialite.dll
+										${GDAL_BIN_DIR}/libmysql.dll
+										${GDAL_BIN_DIR}/geos.dll
+										${GDAL_BIN_DIR}/geos_c.dll
+										${GDAL_BIN_DIR}/libcurl.dll
+										${GDAL_BIN_DIR}/libpng16.dll
+										${GDAL_BIN_DIR}/liblzma.dll
+										${GDAL_BIN_DIR}/openjp2.dll
+										${GDAL_BIN_DIR}/proj_6_3.dll
+										${GDAL_BIN_DIR}/zstd.dll
+										${GDAL_BIN_DIR}/iconv-2.dll
+										${GDAL_BIN_DIR}/iconv.dll
+										${GDAL_BIN_DIR}/freexl.dll
+										${GDAL_BIN_DIR}/zlib1.dll
+										${GDAL_BIN_DIR}/libssl-1_1-x64.dll
+										${GDAL_BIN_DIR}/libcrypto-1_1-x64.dll
+										${GDAL_BIN_DIR}/hdf5_hl.dll
+										${GDAL_BIN_DIR}/hdf5.dll
+										${GDAL_BIN_DIR}/libxml2.dll
+										${GDAL_BIN_DIR}/lwgeom.dll
+										${GDAL_BIN_DIR}/xerces-c_3_2.dll
+			)
+		else()
+			set (  GDAL_DEP_DLL_FILES	${GDAL_BIN_DIR}/expat.dll
+										${GDAL_BIN_DIR}/libpq.dll
+										${GDAL_BIN_DIR}/szip.dll
+										${GDAL_BIN_DIR}/netcdf.dll
+										${GDAL_BIN_DIR}/sqlite3.dll
+										${GDAL_BIN_DIR}/spatialite.dll
+										${GDAL_BIN_DIR}/libmysql.dll
+										${GDAL_BIN_DIR}/geos.dll
+										${GDAL_BIN_DIR}/geos_c.dll
+										${GDAL_BIN_DIR}/libcurl.dll
+										${GDAL_BIN_DIR}/openjp2.dll
+										${GDAL_BIN_DIR}/proj.dll
+										${GDAL_BIN_DIR}/iconv.dll
+										${GDAL_BIN_DIR}/freexl.dll
+										${GDAL_BIN_DIR}/zlib1.dll
+										${GDAL_BIN_DIR}/hdf5_hl.dll
+										${GDAL_BIN_DIR}/hdf5.dll
+										${GDAL_BIN_DIR}/libxml2.dll
+										${GDAL_BIN_DIR}/xerces-c_3_1.dll
+										${GDAL_BIN_DIR}/ssleay32.dll
+										${GDAL_BIN_DIR}/libeay32.dll
+			)
+		endif()
+		
+		#message( STATUS ${GDAL_DLL_FILES} )
+		#message( STATUS ${GDAL_DEP_DLL_FILES} )
 		copy_files("${GDAL_DLL_FILES}" "${CLOUDCOMPARE_DEST_FOLDER}" ) #mind the quotes!
-		copy_files("${GDAL_DLL_FILES2}" "${CLOUDCOMPARE_DEST_FOLDER}" ) #mind the quotes!
+		copy_files("${GDAL_DEP_DLL_FILES}" "${CLOUDCOMPARE_DEST_FOLDER}" ) #mind the quotes!
+
 	endif()
 endfunction()

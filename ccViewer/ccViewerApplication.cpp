67d2beb5eb18a826efad55b4d863fb905a2353bb
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2020-10-17 14:51:30 +0200

    Increasing ccViewer version number

--- ccViewer/ccViewerApplication.cpp
+++ ccViewer/ccViewerApplication.cpp
@@ -26,7 +26,7 @@
 
 
 ccViewerApplication::ccViewerApplication( int &argc, char **argv, bool isCommandLine )
-	: ccApplicationBase( argc, argv, isCommandLine, QStringLiteral( "1.38" ) )
+	: ccApplicationBase( argc, argv, isCommandLine, QStringLiteral( "1.40 alpha" ) )
 {
 	setApplicationName( "CloudCompareViewer" );
 }

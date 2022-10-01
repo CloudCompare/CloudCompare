66430439a39aade3f5ee1bc3a582ec01a994dd18
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2022-10-01 23:44:39 +0200

    Make explicit in the qLASFWFIO cmake file that we rely on lastools' LASZip version

--- plugins/core/IO/qLASFWFIO/cmake/LASLibSupport.cmake
+++ plugins/core/IO/qLASFWFIO/cmake/LASLibSupport.cmake
@@ -4,12 +4,12 @@
 
 # LASLib include directory
 set( LASLIB_INCLUDE_DIR "" CACHE PATH "LASLib include directory" )
-set( LASZIP_INCLUDE_DIR "" CACHE PATH "LASZip include directory" )
+set( LASZIP_LASTOOLS_INCLUDE_DIR "" CACHE PATH "LASZip (from LasTools) include directory" )
 set( LASLIB_RELEASE_LIBRARY "" CACHE FILEPATH "LASLib release library file" )
 set( LASLIB_DEBUG_LIBRARY "" CACHE FILEPATH "LASLib debug library file" )
 
-if ( NOT LASLIB_INCLUDE_DIR OR NOT LASZIP_INCLUDE_DIR )
-	message( SEND_ERROR "No LASLib or LASZip include directories specified (LASLIB_INCLUDE_DIR / LASZIP_INCLUDE_DIR)" )
+if ( NOT LASLIB_INCLUDE_DIR OR NOT LASZIP_LASTOOLS_INCLUDE_DIR )
+	message( SEND_ERROR "No LASLib or LASZip include directories specified (LASLIB_INCLUDE_DIR / LASZIP_LASTOOLS_INCLUDE_DIR)" )
 endif()
 
 # link project with LASLib
@@ -19,7 +19,7 @@ function( target_link_LASLib ) # 1 argument: ARGV0 = project name
 	target_include_directories( ${ARGV0}
 		PRIVATE
 			${LASLIB_INCLUDE_DIR}
-			${LASZIP_INCLUDE_DIR}
+			${LASZIP_LASTOOLS_INCLUDE_DIR}
 	)
 
 	if( LASLIB_RELEASE_LIBRARY )

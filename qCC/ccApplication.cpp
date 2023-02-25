375332f3ee9eab819325878528b60209b2bc98ed
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2023-02-25 19:52:38 +0100

    New 2.13.alpha version (02/25/2023)

--- qCC/ccApplication.cpp
+++ qCC/ccApplication.cpp
@@ -90,7 +90,7 @@ QString ccApplication::GetMinCCVersionForFileVersion(short fileVersion)
 }
 
 ccApplication::ccApplication( int &argc, char **argv, bool isCommandLine )
-	: ccApplicationBase( argc, argv, isCommandLine, QStringLiteral( "2.13.alpha (02/12/2023)" ) )
+	: ccApplicationBase( argc, argv, isCommandLine, QStringLiteral( "2.13.alpha (02/25/2023)" ) )
 {
 	setApplicationName( "CloudCompare" );
 	

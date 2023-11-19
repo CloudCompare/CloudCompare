822674dbebcaa586024e555793802a6898d481df
Paul  RASCLE <paul.rascle@openfields.fr>
2023-11-19 11:50:29 +0100

    Mac os build conda20231118 (#1884)
    * build animation with ffmpeg support on macOS
    
    * macOS build: patch in plugins/core/Standard/qCanupo/contrib/dlib: #define DLIB_NO_GUI_SUPPORT
    
    * macOS build: build plugin qMeshBoolean
    
    * macOS build: post install script, find and add all libraries to bundle, app signature
    
    * Zoom Native gesture on macOS
    
    * Bug on Ransac-SD on macOS (clang compiler): IsLeaf function does not work properly with references on null address
    
    * debug PoissonRecon on macOS: fix following https://github.com/mkazhdan/PoissonRecon/issues/190

--- plugins/core/Standard/qAnimation/extern/QTFFmpegWrapper/cmake/FFmpegSupport.cmake
+++ plugins/core/Standard/qAnimation/extern/QTFFmpegWrapper/cmake/FFmpegSupport.cmake
@@ -61,7 +61,7 @@ function( target_link_ffmpeg ) # 1 argument: ARGV0 = project name
 		if(WIN32)
 			list( APPEND FFMPEG_LIBRARIES ${FFMPEG_LIBRARY_DIR}/${libfile}.lib )
 		elseif( APPLE )
-			list( APPEND FFMPEG_LIBRARIES ${FFMPEG_LIBRARY_DIR}/lib${libfile}.a )
+			list( APPEND FFMPEG_LIBRARIES ${FFMPEG_LIBRARY_DIR}/lib${libfile}.dylib )
 		else()
 			list( APPEND FFMPEG_LIBRARIES ${FFMPEG_LIBRARY_DIR}/lib${libfile}.so )
 		endif()

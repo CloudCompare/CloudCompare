b1be0c021edd56325a725a2fe9a778abf91526f6
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2023-12-02 15:03:42 +0100

    Cloud-to-primitive distances dialog and code improvements

--- qCC/ccPrimitiveDistanceDlg.cpp
+++ qCC/ccPrimitiveDistanceDlg.cpp
@@ -11,54 +11,20 @@
 //#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
 //#  GNU General Public License for more details.                          #
 //#                                                                        #
-//#          COPYRIGHT: Chris Brown                                        #
+//#          COPYRIGHT: CloudCompare Project / Chris Brown                 #
 //#                                                                        #
 //##########################################################################
 
 #include "ccPrimitiveDistanceDlg.h"
 
-//Qt
-#include <QHeaderView>
-#include <QMessageBox>
-
-
-//System
-#include <assert.h>
-
-static bool s_signedDist = true;
-static bool s_flipNormals = false;
-static bool s_treatAsBounded = false;
 ccPrimitiveDistanceDlg::ccPrimitiveDistanceDlg(QWidget* parent)
-	: QDialog(parent, Qt::Tool)
+	: QDialog(parent)
 	, Ui::primitiveDistanceDlg()
 {
 	setupUi(this);
 
-	signedDistCheckBox->setChecked(s_signedDist);
-	flipNormalsCheckBox->setEnabled(s_signedDist);
-	flipNormalsCheckBox->setChecked(s_flipNormals);
-	treatPlanesAsBoundedCheckBox->setUpdatesEnabled(false);
-	treatPlanesAsBoundedCheckBox->setChecked(s_treatAsBounded);
-	connect(cancelButton, &QPushButton::clicked, this, &ccPrimitiveDistanceDlg::cancelAndExit);
-	connect(okButton, &QPushButton::clicked, this, &ccPrimitiveDistanceDlg::applyAndExit);
-	connect(signedDistCheckBox, &QCheckBox::toggled, this, &ccPrimitiveDistanceDlg::toggleSigned);
+	signedDistCheckBox->setChecked(true);
+	flipNormalsCheckBox->setEnabled(true);
+	flipNormalsCheckBox->setChecked(false);
+	treatPlanesAsBoundedCheckBox->setChecked(false);
 }
-
-void ccPrimitiveDistanceDlg::applyAndExit()
-{
-	s_signedDist = signedDistances();
-	s_flipNormals = flipNormals();
-	s_treatAsBounded = treatPlanesAsBounded();
-	accept();
-}
-
-void ccPrimitiveDistanceDlg::cancelAndExit()
-{
-	reject();
-}
-
-void ccPrimitiveDistanceDlg::toggleSigned(bool state)
-{
-	flipNormalsCheckBox->setEnabled(state);
-}
-

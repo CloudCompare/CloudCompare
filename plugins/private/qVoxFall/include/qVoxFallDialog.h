1fbc14b5703bb8d7bf3e73cacddef98771c74dad
Ioannis (Yiannis) Farmakis <i.farmakis@icloud.com>
2024-09-21 15:41:29 +0300

    Update qVoxFallDialog.h

--- plugins/private/qVoxFall/include/qVoxFallDialog.h
+++ plugins/private/qVoxFall/include/qVoxFallDialog.h
@@ -11,7 +11,7 @@
 //#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
 //#  GNU General Public License for more details.                          #
 //#                                                                        #
-//#                             COPYRIGHT: XXX                             #
+//#                 COPYRIGHT: THE UNIVERSITY OF NEWCASTLE                 #
 //#                                                                        #
 //##########################################################################
 

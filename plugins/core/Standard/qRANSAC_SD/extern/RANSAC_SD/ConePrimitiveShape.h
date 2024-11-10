ab1574ef71106506a5a8575b05b3fda3c3682bed
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2024-11-10 21:33:12 +0100

    Syntax fix (missing override statements)

--- plugins/core/Standard/qRANSAC_SD/extern/RANSAC_SD/ConePrimitiveShape.h
+++ plugins/core/Standard/qRANSAC_SD/extern/RANSAC_SD/ConePrimitiveShape.h
@@ -12,81 +12,79 @@ class DLL_LINKAGE ConePrimitiveShape
 : public BitmapPrimitiveShape
 {
 public:
-	size_t Identifier() const;
-	unsigned int RequiredSamples() const { return Cone::RequiredSamples; }
+	size_t Identifier() const override;
+	unsigned int RequiredSamples() const override { return Cone::RequiredSamples; }
 	ConePrimitiveShape(const Cone &cone, float maxRadius = std::numeric_limits<float>::infinity(), float maxAngle = std::numeric_limits<float>::infinity(), float maxLength = std::numeric_limits<float>::infinity());
-	PrimitiveShape* Clone() const;						  
-	float Distance(const Vec3f &p) const;
-	float SignedDistance(const Vec3f &p) const;
-	float NormalDeviation(const Vec3f &p, const Vec3f &n) const;
+	PrimitiveShape* Clone() const override;
+	float Distance(const Vec3f &p) const override;
+	float SignedDistance(const Vec3f &p) const override;
+	float NormalDeviation(const Vec3f &p, const Vec3f &n) const override;
 	void DistanceAndNormalDeviation(const Vec3f &p,
-		const Vec3f &n, std::pair< float, float > *dn) const;
-	void Project(const Vec3f &p, Vec3f *pp) const;
-	void Normal(const Vec3f &p, Vec3f *n) const;
+		const Vec3f &n, std::pair< float, float > *dn) const  override;
+	void Project(const Vec3f &p, Vec3f *pp) const override;
+	void Normal(const Vec3f &p, Vec3f *n) const override;
 	unsigned int ConfidenceTests(unsigned int numTests, float epsilon,
 		float normalThresh, float rms, const PointCloud &pc,
-		const MiscLib::Vector< size_t > &indices) const;
-	void Description(std::string *s) const;
+		const MiscLib::Vector< size_t > &indices) const override;
+	void Description(std::string *s) const override;
 	bool Fit(const PointCloud &pc, float epsilon, float normalThresh,
 		MiscLib::Vector< size_t >::const_iterator begin,
-		MiscLib::Vector< size_t >::const_iterator end);
+		MiscLib::Vector< size_t >::const_iterator end) override;
 	PrimitiveShape *LSFit(const PointCloud &pc, float epsilon,
 		float normalThresh,
 		MiscLib::Vector< size_t >::const_iterator begin,
 		MiscLib::Vector< size_t >::const_iterator end,
-		std::pair< size_t, float > *score) const;
-	LevMarFunc< float > *SignedDistanceFunc() const;
-	void Serialize(std::ostream *o, bool binary = true) const;
-	size_t SerializedSize() const;
-	virtual void Serialize(float* array) const {m_cone.Serialize(array);}
-	virtual size_t SerializedFloatSize() const {return m_cone.SerializedFloatSize();}		
-	void Transform(float scale, const Vec3f &translate);
+		std::pair< size_t, float > *score) const override;
+	LevMarFunc< float > *SignedDistanceFunc() const override;
+	void Serialize(std::ostream *o, bool binary = true) const override;
+	size_t SerializedSize() const override;
+	virtual void Serialize(float* array) const override { m_cone.Serialize(array); }
+	virtual size_t SerializedFloatSize() const override { return m_cone.SerializedFloatSize(); }
+	void Transform(float scale, const Vec3f &translate) override;
 	void Transform(const GfxTL::MatrixXX< 3, 3, float > &rot,
 		const GfxTL::Vector3Df &trans);
-	void Visit(PrimitiveShapeVisitor *visitor) const;
+	void Visit(PrimitiveShapeVisitor *visitor) const override;
 	void SuggestSimplifications(const PointCloud &pc,
 		MiscLib::Vector< size_t >::const_iterator begin,
 		MiscLib::Vector< size_t >::const_iterator end, float distThresh,
-		MiscLib::Vector< MiscLib::RefCountPtr< PrimitiveShape > > *suggestions) const;
+		MiscLib::Vector< MiscLib::RefCountPtr< PrimitiveShape > > *suggestions) const override;
 	bool Similar(float tolerance, const ConePrimitiveShape &shape) const;
 	const Cone &Internal() const { return m_cone; }
 
-	void Parameters(const Vec3f &p,
-		std::pair< float, float > *param) const;
+	void Parameters(const Vec3f &p, std::pair< float, float > *param) const override;
 	void Parameters(GfxTL::IndexedIterator< MiscLib::Vector< size_t >::iterator,
 			PointCloud::const_iterator > begin,
 		GfxTL::IndexedIterator< MiscLib::Vector< size_t >::iterator,
 			PointCloud::const_iterator > end,
-		MiscLib::Vector< std::pair< float, float > > *bmpParams) const;
+		MiscLib::Vector< std::pair< float, float > > *bmpParams) const override;
 	void Parameters(GfxTL::IndexedIterator< IndexIterator,
 			PointCloud::const_iterator > begin,
 		GfxTL::IndexedIterator< IndexIterator,
 			PointCloud::const_iterator > end,
-		MiscLib::Vector< std::pair< float, float > > *bmpParams) const;
-	bool InSpace(float u, float v, Vec3f *p, Vec3f *n) const;
+		MiscLib::Vector< std::pair< float, float > > *bmpParams) const override;
+	bool InSpace(float u, float v, Vec3f *p, Vec3f *n) const override;
 	void BitmapExtent(float epsilon,
 		GfxTL::AABox< GfxTL::Vector2Df > *bbox,
 		MiscLib::Vector< std::pair< float, float > > *params,
-		size_t *uextent, size_t *vextent);
+		size_t *uextent, size_t *vextent) override;
 	void InBitmap(const std::pair< float, float > &param, float epsilon,
 		const GfxTL::AABox< GfxTL::Vector2Df > &bbox, size_t uextent,
-		size_t vextent, std::pair< int, int > *inBmp) const;
+		size_t vextent, std::pair< int, int > *inBmp) const override;
 	void PreWrapBitmap(const GfxTL::AABox< GfxTL::Vector2Df > &bbox,
 		float epsilon, size_t uextent, size_t vextent,
-		MiscLib::Vector< char > *bmp) const;
+		MiscLib::Vector< char > *bmp) const override;
 	void WrapBitmap(const GfxTL::AABox< GfxTL::Vector2Df > &bbox,
-		float epsilon, bool *uwrap, bool *vwrap) const;
+		float epsilon, bool *uwrap, bool *vwrap) const override;
 	void WrapComponents(const GfxTL::AABox< GfxTL::Vector2Df > &bbox,
 		float epsilon, size_t uextent, size_t vextent,
 		MiscLib::Vector< int > *componentImg,
-		MiscLib::Vector< std::pair< int, size_t > > *labels) const;
+		MiscLib::Vector< std::pair< int, size_t > > *labels) const override;
 	void SetExtent(const GfxTL::AABox< GfxTL::Vector2Df > &bbox,
 		const MiscLib::Vector< int > &componentsImg, size_t uextent,
 		size_t vextent, float epsilon, int label);
-	void SetExtent(const GfxTL::AABox< GfxTL::Vector2Df > &bbox);
 	bool InSpace(size_t u, size_t v, float epsilon,
 		const GfxTL::AABox< GfxTL::Vector2Df > &bbox, size_t uextent,
-		size_t vextent, Vec3f *p, Vec3f *n) const;
+		size_t vextent, Vec3f *p, Vec3f *n) const override;
 	bool CheckGeneratedShapeWithinLimits(const PointCloud& pc,
 		MiscLib::Vector< size_t >::const_iterator begin,
 		MiscLib::Vector< size_t >::const_iterator end) override

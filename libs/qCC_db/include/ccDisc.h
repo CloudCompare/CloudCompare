47c5f8a4371d7628599445160fef84c94ced8c10
Paul Leroy <paul.leroy@univ-rennes1.fr>
2025-10-02 08:53:39 +0200

    Update ccDisc.h

--- libs/qCC_db/include/ccDisc.h
+++ libs/qCC_db/include/ccDisc.h
@@ -1,5 +1,7 @@
 // ##########################################################################
 // #                                                                        #
+// #                              CLOUDCOMPARE                              #
+// #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
 // #  it under the terms of the GNU General Public License as published by  #
 // #  the Free Software Foundation; version 2 or later of the License.      #

7abf4bf827817f63ff8386296a5ed446401aa9e2
Romain Janvier <romain.janvier@hotmail.fr>
2025-11-13 15:42:08 +0100

    OpenMP handling

--- cmake/CMakeExternalLibs.cmake
+++ cmake/CMakeExternalLibs.cmake
@@ -37,11 +37,9 @@ add_definitions( -DQT_USE_QSTRINGBUILDER )
 # OpenMP
 # ------------------------------------------------------------------------------
 
-if ( NOT APPLE )
-	find_package(OpenMP QUIET)
-	if (OPENMP_FOUND)
-		message(STATUS "OpenMP found")
-		set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
-		set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-	endif()
+find_package( OpenMP QUIET )
+if( OpenMP_CXX_FOUND )
+    message( STATUS "OpenMP found: compiling CloudCompare with OpenMP support" )
+else()
+    message( STATUS "OpenMP not found..." )
 endif()

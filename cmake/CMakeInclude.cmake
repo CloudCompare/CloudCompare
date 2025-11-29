0e2f61721449ea811e8a23198b6f925745bdcfd0
Romain Janvier <romain.janvier@hotmail.fr>
2025-11-29 15:01:28 +0100

    Remove custom CMake 'install_ext' function

--- cmake/CMakeInclude.cmake
+++ cmake/CMakeInclude.cmake
@@ -4,18 +4,8 @@
 
 # Copy files to the specified directory and for the active configurations
 function( copy_files )	# 2 (or 3) arguments:
-						# ARGV0 = files (if it's a list you have to provide the list alias quoted!)
-						# ARGV1 = target (directory)
+	# ARGV0 = files (if it's a list you have to provide the list alias quoted!)
+	# ARGV1 = target (directory)
 	message(STATUS "Files: ${ARGV0} will be installed in ${ARGV1}" )
 	install( FILES ${ARGV0} DESTINATION ${ARGV1} )
-endfunction()
-
-# Extended 'install' command depending on the build configuration and OS
-# 4 arguments:
-#   - ARGV0 = signature
-#   - ARGV1 = target (warning: one project or one file at a time)
-#   - ARGV2 = base install destination (_debug or _withDebInfo will be automatically appended if multi-conf is supported)
-#   - ARGV3 = install destination suffix (optional)
-function( install_ext )
-	install( ${ARGV0} ${ARGV1} DESTINATION ${ARGV2}${ARGV3} )
 endfunction()
--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -29,11 +29,11 @@ function( InstallSharedLibrary )
 		set( CC_SHARED_LIB_FILENAMES ${CC_SHARED_LIB_FILENAMES} CACHE INTERNAL "" FORCE )
 	endif()
 
-	foreach( destination ${INSTALL_DESTINATIONS} )
-		if( UNIX AND NOT APPLE )
-			set( destination ${LINUX_INSTALL_SHARED_DESTINATION} )
-		endif()
+	if( UNIX AND NOT APPLE )
+		set( destination ${LINUX_INSTALL_SHARED_DESTINATION} )
+	endif()
 
+	foreach( destination ${INSTALL_DESTINATIONS} )
 		_InstallSharedTarget(
 				TARGET ${shared_lib_target}
 				DEST_PATH ${destination}

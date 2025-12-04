5204a6f83870aa0cca687ecaa24a04de0a9348b8
Romain Janvier <romain.janvier@hotmail.fr>
2025-12-04 16:11:33 +0100

    Update BUILD documentation to reflect latest changes to the build system and dependencies.

--- cmake/CMakeExternalLibs.cmake
+++ cmake/CMakeExternalLibs.cmake
@@ -2,16 +2,16 @@
 # Qt
 # ------------------------------------------------------------------------------
 
-find_package( Qt6 REQUIRED
-    COMPONENTS
-        Concurrent
-        Core
-        Gui
-        OpenGL
+find_package( Qt6 6.4 REQUIRED
+	COMPONENTS
+		Concurrent
+		Core
+		Gui
+		OpenGL
 		OpenGLWidgets
-        PrintSupport
-        Svg
-        Widgets
+		PrintSupport
+		Svg
+		Widgets
 )
 
 qt_standard_project_setup()
@@ -28,7 +28,7 @@ add_definitions( -DQT_USE_QSTRINGBUILDER )
 # We could need this for raw openGL calls
 # but we access OpenGL functions through Qt's function pointers...
 #if ( UNIX and NOT APPLE)
-	#set(OpenGL_GL_PREFERENCE GLVND)
+#set(OpenGL_GL_PREFERENCE GLVND)
 #endif()
 #find_package(OpenGL REQUIRED)
 
@@ -39,7 +39,7 @@ add_definitions( -DQT_USE_QSTRINGBUILDER )
 
 find_package( OpenMP QUIET )
 if( OpenMP_CXX_FOUND )
-    message( STATUS "OpenMP found: compiling CloudCompare with OpenMP support" )
+	message( STATUS "OpenMP found: compiling CloudCompare with OpenMP support" )
 else()
-    message( STATUS "OpenMP not found..." )
+	message( STATUS "OpenMP not found..." )
 endif()

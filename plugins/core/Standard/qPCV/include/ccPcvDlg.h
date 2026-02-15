2d85fe2c0e65dfd67e87eecab2b58ee85934dd47
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2026-02-16 00:39:16 +0100

    Fix the PCV plugin after migration to Qt 6 and improve the syntax

--- plugins/core/Standard/qPCV/include/ccPcvDlg.h
+++ plugins/core/Standard/qPCV/include/ccPcvDlg.h
@@ -1,30 +1,32 @@
+#pragma once
+
 //##########################################################################
 //#                                                                        #
-//#                       CLOUDCOMPARE PLUGIN: qPCV                        #
+//#                      CLOUDCOMPARE PLUGIN                               #
 //#                                                                        #
 //#  This program is free software; you can redistribute it and/or modify  #
 //#  it under the terms of the GNU General Public License as published by  #
-//#  the Free Software Foundation; version 2 or later of the License.      #
+//#  the Free Software Foundation; version 2 of the License.               #
 //#                                                                        #
 //#  This program is distributed in the hope that it will be useful,       #
 //#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
 //#  GNU General Public License for more details.                          #
 //#                                                                        #
 //#                  COPYRIGHT: Daniel Girardeau-Montaut                   #
 //#                                                                        #
 //##########################################################################
 
-#ifndef CC_PCV_DLG_HEADER
-#define CC_PCV_DLG_HEADER
-
 #include <ui_pcvDlg.h>
 
-//! Dialog for the PCV plugin
+//! Dialog for the PCV/ShadeVis plugin
 class ccPcvDlg : public QDialog, public Ui::PCVDialog
 {
 public:
-	explicit ccPcvDlg(QWidget* parent = nullptr);
+	explicit ccPcvDlg(QWidget* parent = nullptr)
+		: QDialog(parent, Qt::Tool)
+		, Ui::PCVDialog()
+	{
+		setupUi(this);
+	}
 };
-
-#endif
--- plugins/core/Standard/qPCV/src/PCVContext.cpp
+++ plugins/core/Standard/qPCV/src/PCVContext.cpp
@@ -4,7 +4,8 @@
 //#                                                                        #
 //#  This program is free software; you can redistribute it and/or modify  #
 //#  it under the terms of the GNU Library General Public License as       #
-//#  published by the Free Software Foundation; version 2 or later of the License.  #
+//#  published by the Free Software Foundation; version 2 or later of the  #
+//#  license.                                                              #
 //#                                                                        #
 //#  This program is distributed in the hope that it will be useful,       #
 //#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
@@ -23,7 +24,12 @@
 #include <GenericTriangle.h>
 
 //Qt
+#include <QWindow>
 #include <QOpenGLBuffer>
+#include <QOpenGLContext>
+#include <QOpenGLVersionFunctionsFactory>
+#include <QOpenGLFunctions_2_1>
+#include <QCoreApplication>
 
 //OpenGL
 #ifdef __APPLE__
@@ -40,42 +46,41 @@
 #include <cstring>
 
 //type-less glVertex3Xv call (X=f,d)
-static inline void glVertex3v(const float* v) { glVertex3fv(v); }
-static inline void glVertex3v(const double* v) { glVertex3dv(v); }
-
-//type-less glTranslateX call (X=f,d)
-static inline void glTranslate(float x, float y, float z) { glTranslatef(x, y, z); }
-static inline void glTranslate(double x, double y, double z) { glTranslated(x, y, z); }
-
-//type-less glScaleX call (X=f,d)
-static inline void glScale(float x, float y, float z) { glScalef(x, y, z); }
-static inline void glScale(double x, double y, double z) { glScaled(x, y, z); }
+static inline void GLVertex3v(const float* v, QOpenGLFunctions_2_1* functions) { functions->glVertex3fv(v); }
+static inline void GLVertex3v(const double* v, QOpenGLFunctions_2_1* functions) { functions->glVertex3dv(v); }
 
 using namespace CCCoreLib;
 
 #ifndef ZTWIST
-#define ZTWIST 1e-3f
+#define ZTWIST 1e-3
 #endif
+
+//#define SHOW_LIVE_RENDERING // for tests
+//#define GENERATE_TEST_CAPTURE // for tests
 
 PCVContext::PCVContext()
 	: m_vertices(nullptr)
 	, m_mesh(nullptr)
-	, m_zoom(1)
+	, m_diagonal(0)
+	, m_glSurface(nullptr)
+	, m_glContext(nullptr)
 	, m_pixBuffer(nullptr)
 	, m_width(0)
 	, m_height(0)
-	, m_snapZ(nullptr)
-	, m_snapC(nullptr)
 	, m_meshIsClosed(false)
 {
-	memset(m_viewMat, 0, sizeof(float)*OPENGL_MATRIX_SIZE);
 }
 
 PCVContext::~PCVContext()
 {
 	delete m_pixBuffer;
-	delete[] m_snapZ;
-	delete[] m_snapC;
+	if (m_glContext)
+	{
+		m_glContext->doneCurrent();
+		delete m_glContext;
+		m_glContext = nullptr;
+	}
+	delete m_glSurface;
 }
 
 bool PCVContext::init(unsigned W,
@@ -86,37 +91,25 @@ bool PCVContext::init(unsigned W,
 {
 	assert(!m_pixBuffer);
 
-	m_pixBuffer = new QOpenGLBuffer(QOpenGLBuffer::PixelPackBuffer);
-	if (!m_pixBuffer->create())
-	{
-		delete m_pixBuffer;
-		m_pixBuffer = nullptr;
-		return false;
-	}
-	m_pixBuffer->allocate(W * H);
-
-	unsigned size = W*H;
-	m_snapZ = new float[size];
-	if (!m_snapZ)
-	{
-		delete m_pixBuffer;
-		m_pixBuffer = nullptr;
-		return false;
-	}
-
 	m_meshIsClosed = (closedMesh || !mesh);
-	if (!m_meshIsClosed)
+
+	unsigned size = W * H;
+	try
 	{
-		//buffer for color
-		m_snapC = new unsigned char[4*size];
-		if (!m_snapC)
+		// depth buffer
+		m_snapZ.resize(size, 0);
+#if not defined(SHOW_LIVE_RENDERING) && not defined(GENERATE_TEST_CAPTURE)
+		if (!m_meshIsClosed)
+#endif
 		{
-			delete m_pixBuffer;
-			m_pixBuffer = nullptr;
-			delete[] m_snapZ;
-			m_snapZ = nullptr;
-			return false;
+			// color buffer
+			m_snapC.resize(size * 4, 0);
 		}
+	}
+	catch (const std::bad_alloc)
+	{
+		// not enough memory
+		return false;
 	}
 
 	m_width = W;
@@ -124,136 +117,160 @@ bool PCVContext::init(unsigned W,
 
 	associateToEntity(cloud, mesh);
 
-	glInit();
+	if (!glInit())
+	{
+		// failed to initialize the GL context
+		return false;
+	}
+
+	if (!makeCurrent())
+	{
+		return false;
+	}
+
+	auto* functions = QOpenGLVersionFunctionsFactory::get<QOpenGLFunctions_2_1>(m_glContext);
+	if (!functions)
+	{
+		// OpenGL version not supported
+		return false;
+	}
+	functions->initializeOpenGLFunctions();
+	functions->glPixelStorei(GL_PACK_ROW_LENGTH, 0);
+	functions->glPixelStorei(GL_PACK_ALIGNMENT, 1);
+	functions->glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	m_pixBuffer = new QOpenGLBuffer(QOpenGLBuffer::PixelPackBuffer);
+	if (!m_pixBuffer->create())
+	{
+		delete m_pixBuffer;
+		m_pixBuffer = nullptr;
+		return false;
+	}
+
+	m_pixBuffer->setUsagePattern(QOpenGLBuffer::StreamRead);
+
+	// As per Qt's doc: The buffer must be bound to the same QOpenGLContext current when create() was called,
+	// or to another QOpenGLContext that is sharing with it. Otherwise, false will be returned from this function.
+	if (!m_pixBuffer->bind())
+	{
+		return false;
+	}
+
+	// As per Qt's doc: It is assumed that create() has been called on this buffer
+	// and that it has been bound to the current context.
+	m_pixBuffer->allocate(W * H * sizeof(float));
+
+	m_pixBuffer->release();
 
 	return true;
 }
 
 void PCVContext::associateToEntity(GenericCloud* cloud, GenericMesh* mesh)
 {
-	assert(cloud);
+	m_vertices = nullptr;
+	m_diagonal = static_cast<PointCoordinateType>(std::max(m_width, m_height));
+	m_viewCenter = CCVector3(0, 0, 0);
+
 	if (!cloud)
+	{
+		assert(false);
 		return;
+	}
 
 	m_vertices = cloud;
 	m_mesh = mesh;
 
-	//we get cloud bounding box
+	//we retrieve the cloud bounding box
 	CCVector3 bbMin;
 	CCVector3 bbMax;
 	m_vertices->getBoundingBox(bbMin, bbMax);
 
-	//we compute bbox diagonal
-	PointCoordinateType maxD = (bbMax - bbMin).norm();
+	//we compute the bbox diagonal
+	m_diagonal = (bbMax - bbMin).norm();
 
-	//we deduce default zoom
-	m_zoom = (CCCoreLib::GreaterThanEpsilon( maxD ) ? static_cast<PointCoordinateType>(std::min(m_width, m_height)) / maxD : CCCoreLib::PC_ONE);
-
-	//as well as display center
-	m_viewCenter = (bbMax+bbMin)/2;
+	//as well as the display center
+	m_viewCenter = (bbMax + bbMin) / 2;
 }
 
-void PCVContext::glInit()
+bool PCVContext::makeCurrent()
 {
-	if (!m_pixBuffer || !m_pixBuffer->isCreated())
-		return;
-
-	m_pixBuffer->bind();
-
-	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
-	glDepthMask(GL_TRUE);
-	glDisable(GL_LIGHTING);
-
-	glPixelStorei(GL_PACK_ROW_LENGTH, 0);
-	glPixelStorei(GL_PACK_ALIGNMENT, 1);
-
-	//model view matrix initialization
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glGetFloatv(GL_MODELVIEW_MATRIX, m_viewMat);
-	glPushMatrix();
-
-	//projection matrix initialization
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	float w2 = 0.5f * m_width;
-	float h2 = 0.5f * m_height;
-	float maxD = static_cast<float>(std::max(m_width, m_height));
-	glOrtho(-w2, w2, -h2, h2, -maxD, maxD);
-	glPushMatrix();
-}
-
-void PCVContext::setViewDirection(const CCVector3& V)
-{
-	if (!m_pixBuffer || !m_pixBuffer->isCreated())
-		return;
-
-	m_pixBuffer->bind();
-
-	glMatrixMode(GL_MODELVIEW);
-	glPushMatrix();
-	glLoadIdentity();
-
-	CCVector3 U(0, 0, 1);
-	if (1 - std::abs(V.dot(U)) < 1.0e-4)
+	if (!m_glContext || !m_glSurface)
 	{
-		U.y = 1;
-		U.z = 0;
+		return false;
+	}
+	if (!m_glContext->makeCurrent(m_glSurface))
+	{
+        return false;
+	}
+	return true;
+}
+
+bool PCVContext::glInit()
+{
+	if (!m_glContext)
+	{
+		m_glContext = new QOpenGLContext;
+		if (!m_glContext->create())
+		{
+			delete m_glContext;
+			m_glContext = nullptr;
+			return false;
+		}
 	}
 
-	gluLookAt(-V.x, -V.y, -V.z, 0.0, 0.0, 0.0, U.x, U.y, U.z);
-	glGetFloatv(GL_MODELVIEW_MATRIX, m_viewMat);
-	glPopMatrix();
+	if (!m_glSurface)
+	{
+		QWindow* surface = new QWindow;
+		surface->setSurfaceType(QSurface::OpenGLSurface);
+		surface->setFormat(m_glContext->format());
+		surface->create();
+		surface->resize(m_width, m_height);
+#ifdef SHOW_LIVE_RENDERING
+		surface->show();
+		QCoreApplication::processEvents();
+#endif
+		m_glSurface = surface;
+	}
+
+	return true;
 }
 
 void PCVContext::drawEntity()
 {
-	assert(m_vertices);
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadMatrixf(m_viewMat);
-	glScale(m_zoom, m_zoom, m_zoom);
-	glTranslate(-m_viewCenter.x, -m_viewCenter.y, -m_viewCenter.z);
-
-	glColor3ub(255, 255, 0); //yellow by default
-
+	auto* functions = QOpenGLVersionFunctionsFactory::get<QOpenGLFunctions_2_1>(m_glContext);
+	if (!functions)
+	{
+		// OpenGL version not supported
+		return;
+	}
+	
 	if (m_mesh)
 	{
 		unsigned nTri = m_mesh->size();
 		m_mesh->placeIteratorAtBeginning();
 
-		glBegin(GL_TRIANGLES);
+		functions->glBegin(GL_TRIANGLES);
 		for (unsigned i = 0; i < nTri; ++i)
 		{
 			const GenericTriangle* t = m_mesh->_getNextTriangle();
-			glVertex3v(t->_getA()->u);
-			glVertex3v(t->_getB()->u);
-			glVertex3v(t->_getC()->u);
+			GLVertex3v(t->_getA()->u, functions);
+			GLVertex3v(t->_getB()->u, functions);
+			GLVertex3v(t->_getC()->u, functions);
 		}
-		glEnd();
+		functions->glEnd();
 	}
-	else
+	else if (m_vertices)
 	{
 		unsigned nPts = m_vertices->size();
 		m_vertices->placeIteratorAtBeginning();
 
-		glBegin(GL_POINTS);
+		functions->glBegin(GL_POINTS);
 		for (unsigned i = 0; i < nPts; ++i)
-			glVertex3v(m_vertices->getNextPoint()->u);
-		glEnd();
+		{
+			GLVertex3v(m_vertices->getNextPoint()->u, functions);
+		}
+		functions->glEnd();
 	}
-}
-
-void openGLSnapshot(GLenum format, GLenum type, void* buffer)
-{
-	assert(buffer);
-
-	int vp[4];
-	glGetIntegerv(GL_VIEWPORT, vp);
-
-	glReadPixels(vp[0], vp[1], vp[2], vp[3], format, type, buffer);
 }
 
 //The method below is inspired from ShadeVis' "GLAccumPixel" (Cignoni et al.)
@@ -265,7 +282,7 @@ void openGLSnapshot(GLenum format, GLenum type, void* buffer)
 * Visual Computing Lab                                            /\/|      *
 * ISTI - Italian National Research Council                           |      *
 *****************************************************************************/
-int PCVContext::GLAccumPixel(std::vector<int>& visibilityCount)
+int PCVContext::glAccumPixel(std::vector<int>& visibilityCount, const CCVector3d& viewDir)
 {
 	if (!m_pixBuffer || !m_pixBuffer->isCreated())
 		return -1;
@@ -274,43 +291,121 @@ int PCVContext::GLAccumPixel(std::vector<int>& visibilityCount)
 	if (m_vertices->size() != visibilityCount.size())
 		return -1;
 
-	assert(m_snapZ);
+	assert(m_snapZ.data());
 
-	m_pixBuffer->bind();
+	if (!makeCurrent())
+	{
+		return -2;
+	}
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glDepthRange(2.0f*ZTWIST, 1.0f);
+	auto* functions = QOpenGLVersionFunctionsFactory::get<QOpenGLFunctions_2_1>(m_glContext);
+	if (!functions)
+	{
+		// OpenGL version not supported
+		return -3;
+	}
 
-	if (m_meshIsClosed)
-		glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
-	else
-		glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+	functions->glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	functions->glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//functions->glDepthRange(2.0*ZTWIST, 1.0);
+
+	functions->glPointSize(1.0f);
+	functions->glDisable(GL_BLEND);
+	functions->glEnable(GL_DEPTH_TEST);
+	functions->glEnable(GL_CULL_FACE);
+	functions->glDisable(GL_LIGHTING);
+
+	//projection matrix initialization
+	{
+		functions->glMatrixMode(GL_PROJECTION);
+		functions->glLoadIdentity();
+		double ar = static_cast<double>(m_height) / m_width;
+		double xMax = m_diagonal/2;
+		double yMax = xMax * ar;
+		functions->glOrtho(-xMax, xMax, -yMax, yMax, -xMax, xMax);
+	}
+	double projectionMat[OPENGL_MATRIX_SIZE];
+	functions->glGetDoublev(GL_PROJECTION_MATRIX, projectionMat);
+
+	//model view matrix initialization
+	{
+		functions->glMatrixMode(GL_MODELVIEW);
+		functions->glLoadIdentity();
+
+		CCVector3d U(0.0, 0.0, 1.0);
+		if (1.0 - std::abs(viewDir.dot(U)) < 1.0e-4)
+		{
+			// swap Y and Z
+			U.y = 1.0;
+			U.z = 0.0;
+		}
+
+		gluLookAt(	m_viewCenter.x+-viewDir.x, m_viewCenter.y-viewDir.y, m_viewCenter.z-viewDir.z,
+					m_viewCenter.x, m_viewCenter.y, m_viewCenter.z,
+					U.x, U.y, U.z );
+	}
+	double viewMat[OPENGL_MATRIX_SIZE];
+	functions->glGetDoublev(GL_MODELVIEW_MATRIX, viewMat);
+
+	functions->glColor3ub(255, 255, 0); //yellow by default
+
+	GLboolean drawColor = GL_TRUE;
+#if not defined(GENERATE_TEST_CAPTURE) && not defined(SHOW_LIVE_RENDERING)
+	drawColor = m_meshIsClosed ? GL_FALSE : GL_TRUE;
+#endif
+	functions->glColorMask(drawColor, drawColor, drawColor, drawColor);
+
+	int viewPort[4]{0, 0, static_cast<int>(m_width), static_cast<int>(m_height)};
+	functions->glViewport(viewPort[0], viewPort[1], viewPort[2], viewPort[3]);
 
 	//display 3D entity (front)
-	glCullFace(GL_BACK);
+	functions->glCullFace(GL_BACK);
 	drawEntity();
 
-	if (!m_meshIsClosed)
+	if (m_mesh && !m_meshIsClosed)
 	{
 		//display it again (back)
-		glCullFace(GL_FRONT);
+		functions->glCullFace(GL_FRONT);
 		drawEntity();
 
-		assert(m_snapC);
-		openGLSnapshot(GL_RGBA, GL_UNSIGNED_BYTE, m_snapC);
+		if (!m_pixBuffer->bind())
+		{
+			return -4;
+		}
+		functions->glReadPixels(viewPort[0], viewPort[1], viewPort[2], viewPort[3], GL_RGBA, GL_UNSIGNED_BYTE, nullptr/*m_snapC.data()*/);
+		m_pixBuffer->read(0, m_snapC.data(), static_cast<int>(m_width * m_height * 4));
+		m_pixBuffer->release();
 	}
-	openGLSnapshot(GL_DEPTH_COMPONENT, GL_FLOAT, m_snapZ);
+#ifdef GENERATE_TEST_CAPTURE
+	else
+	{
+		if (!m_pixBuffer->bind())
+		{
+			return -4;
+		}
+		functions->glReadPixels(viewPort[0], viewPort[1], viewPort[2], viewPort[3], GL_BGRA, GL_UNSIGNED_BYTE, nullptr/*m_snapC.data()*/);
+		m_pixBuffer->read(0, m_snapC.data(), static_cast<int>(m_width * m_height * 4));
+		m_pixBuffer->release();
 
-	if (m_meshIsClosed)
-		glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+		QImage image(m_snapC.data(), m_width, m_height, QImage::Format::Format_ARGB32);
+		image.save("E:/Temp/capture.png");
+	}
+#endif
 
-	glDepthRange(0, 1.0f - 2.0f*ZTWIST);
-	double MM[OPENGL_MATRIX_SIZE];
-	glGetDoublev(GL_MODELVIEW_MATRIX, MM);
-	double MP[OPENGL_MATRIX_SIZE];
-	glGetDoublev(GL_PROJECTION_MATRIX, MP);
-	int VP[4];
-	glGetIntegerv(GL_VIEWPORT, VP);
+	// Retrieve Z-buffer
+	{
+		if (!m_pixBuffer->bind())
+		{
+			return -4;
+		}
+		functions->glReadPixels(viewPort[0], viewPort[1], viewPort[2], viewPort[3], GL_DEPTH_COMPONENT, GL_FLOAT, nullptr /*m_snapZ.data()*/);
+		m_pixBuffer->read(0, m_snapZ.data(), static_cast<int>(m_width * m_height * sizeof(float)));
+		m_pixBuffer->release();
+	}
+
+#ifdef SHOW_LIVE_RENDERING
+	m_glContext->swapBuffers(m_glSurface);
+#endif
 
 	int count = 0;
 	int sx4 = (m_width << 2);
@@ -324,7 +419,7 @@ int PCVContext::GLAccumPixel(std::vector<int>& visibilityCount)
 		double tx = 0.0;
 		double ty = 0.0;
 		double tz = 0.0;
-		gluProject(P->x, P->y, P->z, MM, MP, VP, &tx, &ty, &tz);
+		gluProject(P->x, P->y, P->z, viewMat, projectionMat, viewPort, &tx, &ty, &tz);
 
 		int txi = static_cast<int>(floor(tx));
 		int tyi = static_cast<int>(floor(ty));
@@ -338,7 +433,7 @@ int PCVContext::GLAccumPixel(std::vector<int>& visibilityCount)
 			{
 				//int c1 = std::max(m_snapC[dec],m_snapC[dec<<2+4]);
 				//int c2 = std::max(m_snapC[dec<<2+sx4],m_snapC[dec<<2+sx4+4]);
-				const unsigned char* pix = m_snapC + (dec << 2);
+				const auto* pix = m_snapC.data() + (static_cast<size_t>(dec) << 2);
 				int c1 = std::max(pix[0], pix[4]);
 				pix += sx4;
 				int c2 = std::max(pix[0], pix[4]);
@@ -356,6 +451,9 @@ int PCVContext::GLAccumPixel(std::vector<int>& visibilityCount)
 			}
 		}
 	}
+
+	assert(m_glContext);
+	m_glContext->doneCurrent();
 
 	return count;
 }
